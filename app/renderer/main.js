e87e269f2ad36b9be82b6cc442363d27611f8579
akashnimare <svnitakash@gmail.com>
2016-06-28 16:04:01 +0530

    Prompt UI + placeholder Updated

--- app/renderer/main.js
+++ app/renderer/main.js
@@ -1,19 +1,21 @@
-		window.onload = function getURL () {
+		window.onload = function getURL() {
 
-		var shell = require('electron').shell;
-		var JsonDB = require('node-json-db');
-		var db = new JsonDB("domain", true, true);
-		var data = db.getData("/");
+		    var shell = require('electron').shell;
+		    var JsonDB = require('node-json-db');
+		    var db = new JsonDB("domain", true, true);
+		    var data = db.getData("/");
 
-		if (data["domain"] !== undefined) {
-			window.location.href = data["domain"];
+		    if (data["domain"] !== undefined) {
+		        window.location.href = 'https://' + data["domain"];
+		    } else {
+		        var dialogs = require('dialogs')()
+		        dialogs.prompt('Enter the URL for your Zulip server', function(ok) {
+		            db.push("/domain", ok);
+		            console.log(db);
+		            window.location.href = 'https://' + ok;
+		        })
+		    }
+
+		var addPlaceHolder = document.getElementsByTagName('input')[0];
+		addPlaceHolder.setAttribute('placeholder', 'zulip.example.com');
 		}
-		else {
-			var dialogs = require('dialogs')()
-  			dialogs.prompt('Add your Zulip Domain', function (ok) {
-  			db.push("/domain", ok);
-  			console.log(db);
-        	window.location.href = ok;
-      		})
-		}
-}
