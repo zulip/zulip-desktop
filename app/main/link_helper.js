320f05b408373764aa820fabde671f1f6a43afee
Akash Nimare <akashnimare@users.noreply.github.com>
2016-07-17 03:40:12 +0530

    :runner: Fixed #11

--- app/main/link_helper.js
+++ app/main/link_helper.js
@@ -2,8 +2,8 @@ const wurl = require('wurl');
 
 // Check link if it's internal/external
 function linkIsInternal(currentUrl, newUrl) {
-    var currentDomain = wurl('domain', currentUrl);
-    var newDomain = wurl('domain', newUrl);
+    var currentDomain = wurl('thisDomain', currentUrl);
+    var newDomain = wurl('thisDomain', newUrl);
     return currentDomain === newDomain;
 }
 
