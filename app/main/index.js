435df455aaabf4412356b05877ac438d93f55b9c
akashnimare <svnitakash@gmail.com>
2016-07-29 10:00:39 +0530

    :zap: Restore window size and position

--- app/main/index.js
+++ app/main/index.js
@@ -2,19 +2,22 @@
 const path = require('path');
 const electron = require('electron');
 const {app, shell} = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
 const ipc = require('electron').ipcMain;
+const Configstore = require('configstore');
+const JsonDB = require('node-json-db');
 const tray = require('./tray');
 const link = require ('./link_helper');
 const {linkIsInternal} = link;
 
-const electronLocalshortcut = require('electron-localshortcut');
-const JsonDB = require('node-json-db');
 const db = new JsonDB("domain", true, true);
 const data = db.getData("/");
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 require('electron-context-menu')();
+
+const conf = new Configstore("Zulip-Desktop");
 
 // prevent window being garbage collected
 let mainWindow;
@@ -23,15 +26,12 @@ let targetLink;
 // Load this url in main window
 const targetUrl = 'file://' + path.join(__dirname, '../renderer', 'index.html');
 
-
 function checkWindowURL() {
 	if (data["domain"] !== undefined) {
 		return data["domain"]
 	}
 	return targetLink
 }
-
-
 
 const APP_ICON = path.join(__dirname, '../resources', 'Icon');
 
@@ -40,7 +40,6 @@ const iconPath = () => {
     ? APP_ICON + '.ico'
     : APP_ICON + '.png'
 };
-
 
 function onClosed() {
 	// dereference the window
@@ -52,16 +51,51 @@ function createMainWindow() {
 	const win = new electron.BrowserWindow({
 		// This settings needs to be saved in config
 		title: 'Zulip',
-		width: 1000,
-		height: 600,
+		width: conf.get('width') || 1000,
+		height: conf.get('height') || 600,
 		icon: iconPath(),
-		minWidth: 800,
-		minHeight: 600
+		minWidth: 600,
+		minHeight: 400
 	});
 
 	win.loadURL(targetUrl);
 	win.on('closed', onClosed);
 	win.setTitle('Zulip');
+
+	// Let's save browser window position 
+	if (conf.get('x') || conf.get('y')) {
+	  win.setPosition(conf.get('x'), conf.get('y'));
+	}
+
+	if (conf.get('maximize')) {
+	  win.maximize();
+	}
+
+	// Handle sizing events so we can persist them.
+	win.on('maximize', function (event) {
+	  conf.set('maximize', true);
+	});
+
+	win.on('unmaximize', function (event) {
+	  conf.set('maximize', false);
+	});
+
+	win.on('resize', function (event) {
+	  var size = this.getSize();
+	  conf.set({
+	    width: size[0],
+	    height: size[1]
+	  });
+	});
+
+	// on osx it's 'moved'
+	win.on('move', function (event) {
+	  var pos = this.getPosition();
+	  conf.set({
+	    x: pos[0],
+	    y: pos[1]
+	  });
+	});
 
 	// stop page to update it's title
 	win.on('page-title-updated', (e) => {
