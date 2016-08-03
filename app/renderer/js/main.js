1d7ee0d2aa54b49e5ee55254c2511878ef2c9a7a
akashnimare <svnitakash@gmail.com>
2016-08-03 18:18:07 +0530

    Remove spellcheck for forms

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -1,34 +1,37 @@
-		window.onload = function getURL() {
-			const request = require('request');
-			const JsonDB = require('node-json-db');
-			const ipcRenderer = require('electron').ipcRenderer;
-			const dialogs = require('dialogs')()
-		    const db = new JsonDB("domain", true, true);
-		    const data = db.getData("/");
+window.onload = function getURL() {
+	const request = require('request');
+	const JsonDB = require('node-json-db');
+	const ipcRenderer = require('electron').ipcRenderer;
+	const dialogs = require('dialogs')()
+    const db = new JsonDB("domain", true, true);
+    const data = db.getData("/");
 
-		    if (data["domain"] !== undefined) {
-		        window.location.href = data["domain"];
-		    } else {
+    if (data["domain"] !== undefined) {
+        window.location.href = data["domain"];
+    } else {
 
-		        dialogs.prompt('Enter the URL for your Zulip server', function(url) {
+        dialogs.prompt('Enter the URL for your Zulip server', function(url) {
 
-		        	let newurl = 'https://' + url.replace(/^https?:\/\//,'')
-		        	let checkURL = newurl + '/static/audio/zulip.ogg';
+        	let newurl = 'https://' + url.replace(/^https?:\/\//,'')
+        	let checkURL = newurl + '/static/audio/zulip.ogg';
 
-		        	request(checkURL, function (error, response, body) {
-		        		if (!error && response.statusCode !== 404) {
-		        			    db.push("/domain", newurl);
-		        			    ipcRenderer.send('new-domain', newurl);
-		        			    window.location.href = newurl ;
-		        		}
-		        		else {
-		        			dialogs.alert('Not valid url');
-		        			console.log("Not valid url");
-		        		}
-		        	})
-		        })
-		    }
+        	request(checkURL, function (error, response, body) {
+        		if (!error && response.statusCode !== 404) {
+        			    db.push("/domain", newurl);
+        			    ipcRenderer.send('new-domain', newurl);
+        			    window.location.href = newurl ;
+        		}
+        		else {
+        			dialogs.alert('Not valid url');
+        			console.log("Not valid url");
+        		}
+        	})
+        })
+    }
 
-		const addPlaceHolder = document.getElementsByTagName('input')[0];
-		addPlaceHolder.setAttribute('placeholder', 'zulip.example.com');
-		}
+const getInput = document.getElementsByTagName('input')[0];
+
+getInput.setAttribute('placeholder', 'zulip.example.com'); // add placeholder
+getInput.setAttribute('spellcheck', 'false');	// no spellcheck for form	
+
+}
