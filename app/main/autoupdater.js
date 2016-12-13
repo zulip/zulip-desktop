98bd4fd9b9e088c037d5da1a3b3d97d720cf92d3
akashnimare <svnitakash@gmail.com>
2016-12-14 00:55:29 +0530

    :fire:

--- app/main/autoupdater.js
+++ app/main/autoupdater.js
@@ -1,114 +1,8 @@
-// const {app, autoUpdater, dialog} = require('electron');
-// const version = app.getVersion();
-// const os = require('os');
-// const platform = os.platform() + '_' + os.arch();  // usually returns darwin_64
-
-
-
-// const updaterFeedURL = 'http://zulipdesktop.herokuapp.com/update/'+ platform + '/' + version;
-
-// function appUpdater() {
-
-// 	autoUpdater.setFeedURL(updaterFeedURL);
-
-// 	// Log whats happening
-// 	autoUpdater.on('error', err => console.log(err))
-//     autoUpdater.on('checking-for-update', () => console.log('checking-for-update'))
-// 	autoUpdater.on('update-available', () => console.log('update-available'))
-// 	autoUpdater.on('update-not-available', () => console.log('update-not-available'))
-
-// 	// Ask the user if update is available
-// 	autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName, releaseDate) => {
-
-// 		let message = app.getName() + ' ' + releaseName + ' is now available. It will be installed the next time you restart the application.';
-// 		if (releaseNotes) {
-// 			let splitNotes = releaseNotes.split(/[^\r]\n/);
-// 			message += '\n\nRelease notes:\n';
-// 			splitNotes.forEach(function (notes) {
-// 				message += notes + '\n\n';
-// 			})
-// 		};
-// 		// Ask user to update the app
-// 		dialog.showMessageBox({
-// 			type: 'question',
-// 		    buttons: ['Install and Relaunch' ,'Cancel'],
-// 		    defaultId: 0,
-// 		    message: 'A new version of ' + app.getName() + ' has been downloaded',
-// 		    detail: message
-// 		  }, response => {
-// 		  	if (response === 0) {
-// 		  		setTimeout(() => autoUpdater.quitAndInstall(), 1);
-// 		  	}
-// 		  })
-// 	})
-
-// 	// init for updates
-// 	autoUpdater.checkForUpdates()
-// }
-
-// exports = module.exports = {
-// 	appUpdater
-// }
-
-
-// const {app, autoUpdater, dialog} = require('electron');
-// const version = app.getVersion();
-// const os = require('os');
-// const platform = os.platform() + '_' + os.arch();  // usually returns darwin_64
-
-
-
-// const updaterFeedURL = 'http://zulipdesktop.herokuapp.com/update/'+ platform + '/' + version;
-
-// function appUpdater() {
-
-// 	autoUpdater.setFeedURL(updaterFeedURL);
-
-// 	// Log whats happening
-// 	autoUpdater.on('error', err => console.log(err))
-//     autoUpdater.on('checking-for-update', () => console.log('checking-for-update'))
-// 	autoUpdater.on('update-available', () => console.log('update-available'))
-// 	autoUpdater.on('update-not-available', () => console.log('update-not-available'))
-
-// 	// Ask the user if update is available
-// 	autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName, releaseDate) => {
-
-// 		let message = app.getName() + ' ' + releaseName + ' is now available. It will be installed the next time you restart the application.';
-// 		if (releaseNotes) {
-// 			let splitNotes = releaseNotes.split(/[^\r]\n/);
-// 			message += '\n\nRelease notes:\n';
-// 			splitNotes.forEach(function (notes) {
-// 				message += notes + '\n\n';
-// 			})
-// 		};
-// 		// Ask user to update the app
-// 		dialog.showMessageBox({
-// 			type: 'question',
-// 		    buttons: ['Install and Relaunch' ,'Cancel'],
-// 		    defaultId: 0,
-// 		    message: 'A new version of ' + app.getName() + ' has been downloaded',
-// 		    detail: message
-// 		  }, response => {
-// 		  	if (response === 0) {
-// 		  		setTimeout(() => autoUpdater.quitAndInstall(), 1);
-// 		  	}
-// 		  })
-// 	})
-
-// 	// init for updates
-// 	autoUpdater.checkForUpdates()
-// }
-
-// exports = module.exports = {
-// 	appUpdater
-// }
-
+'use strict';
 const {app, autoUpdater, dialog} = require('electron');
 const version = app.getVersion();
 const os = require('os');
 const platform = os.platform() + '_' + os.arch();  // usually returns darwin_64
-
-
 
 const updaterFeedURL = 'http://zulipdesktop.herokuapp.com/update/'+ platform + '/' + version;
 
@@ -146,7 +40,6 @@ function appUpdater() {
 		  	}
 		  })
 	})
-
 	// init for updates
 	autoUpdater.checkForUpdates()
 }
@@ -154,5 +47,3 @@ function appUpdater() {
 exports = module.exports = {
 	appUpdater
 }
-
-
