dc15edf0cdf1e6738131453b90cbb460e32c886e
akashnimare <svnitakash@gmail.com>
2016-12-14 03:02:01 +0530

    Unsubscribe spellchekcer

--- app/main/spellchecker.js
+++ app/main/spellchecker.js
@@ -12,3 +12,10 @@ let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler);
 let contextMenuListener = new ContextMenuListener((info) => {
   contextMenuBuilder.showPopupMenu(info);
 });
+
+// Clean up events after you navigate away from this page;
+// otherwise, you may experience errors
+window.addEventListener('beforeunload', function() {
+    spellCheckHandler.unsubscribe();
+    contextMenuListener.unsubscribe();
+});
