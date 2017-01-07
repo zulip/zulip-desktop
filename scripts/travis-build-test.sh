9e75861546a28ac21fc78e1246236111f1a78034
Akash Nimare <akashnimare@users.noreply.github.com>
2017-01-07 17:50:56 +0530

    Fixing travis test (linux)

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export DISPLAY=:99.0
+  export {no_proxy,NO_PROXY}="127.0.0.1,localhost"
+	export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
   sleep 3
 fi
