b4fb00aa524c6183fde7c12cbd959ba08ae11bbe
akashnimare <svnitakash@gmail.com>
2017-03-02 16:47:41 +0530

    :bug: show error message if input server url is null #116

--- app/main/domain.js
+++ app/main/domain.js
@@ -8,29 +8,26 @@ const data = db.getData('/');
 
 console.log(data.domain);
 
-// if (data.domain && window.location.href.indexOf(data.domain) === -1) {
-// 	window.location.href = data.domain
-// }
-// require('electron-connect').client.create();
-
 window.addDomain = function () {
-	document.getElementById('main').innerHTML = 'checking...';
-
 	let newDomain = document.getElementById('url').value;
 	newDomain = newDomain.replace(/^https?:\/\//, '');
 
 	const domain = 'https://' + newDomain;
 	const checkDomain = domain + '/static/audio/zulip.ogg';
-
-	request(checkDomain, (error, response) => {
-		if (!error && response.statusCode !== 404) {
-			document.getElementById('main').innerHTML = 'Connect';
-			db.push('/domain', domain);
-			ipcRenderer.send('new-domain', domain);
-		} else {
-			document.getElementById('main').innerHTML = 'Connect';
-			document.getElementById('server-status').innerHTML = 'Not a valid Zulip Server.';
-		}
-	});
+	if (newDomain === '') {
+		document.getElementById('server-status').innerHTML = 'Please input a value';
+	} else {
+		document.getElementById('main').innerHTML = 'checking...';
+		request(checkDomain, (error, response) => {
+			if (!error && response.statusCode !== 404) {
+				document.getElementById('main').innerHTML = 'Connect';
+				db.push('/domain', domain);
+				ipcRenderer.send('new-domain', domain);
+			} else {
+				document.getElementById('main').innerHTML = 'Connect';
+				document.getElementById('server-status').innerHTML = 'Not a valid Zulip Server.';
+			}
+		});
+	}
 };
 
