4c91bebe6a81545aaa7932b8a7dcedf6198f0b26
Lplenka <laluprasad.lenka95@gmail.com>
2017-03-21 10:57:28 +0530

    Scraped Unread count from webpage avoiding webContent  object null conditions

--- app/main/tray.js
+++ app/main/tray.js
@@ -5,6 +5,7 @@ const app = require('electron').app;
 const {addDomain, about} = require('./windowmanager');
 
 let tray = null;
+let unread = 0;
 
 const APP_ICON = path.join(__dirname, '../resources/tray', 'tray');
 
@@ -14,6 +15,41 @@ const iconPath = () => {
 	}
 	return APP_ICON + (process.platform === 'win32' ? 'win.ico' : 'osx.png');
 };
+
+app.on('web-contents-created', (e, wb) => {
+	setTimeout(() => {
+		setInterval(() => {
+			// If to handle error if wb is destroyed before 7 seconds
+			if (wb !== null) {
+				 // If to handle error if wb is destroyed before entering this block happens generally during switching server
+				if (!wb.isDestroyed()) {
+					wb.executeJavaScript("document.querySelector('#streams_header')", res => {
+				// If to handle error if wb is destroyed before entering this block and res to check it's a zulip chat web page
+						if (res !== null && wb !== null) {
+							console.log("I am inside");
+							wb.executeJavaScript("document.querySelector('.value').innerText", result => {
+							//If to check if value class has no innerHTML
+								if (result.length)
+									unread = result;
+								else
+									unread = 0;
+							});
+						}
+					});
+				}
+			}
+
+		}, 500);//setInterval updates unread every 0.5 seconds
+
+	}, 7000); //delay so that the functions starts after webpage is completely loaded
+
+});
+
+
+
+
+
+
 
 const createHandler = () => {
 	const contextMenu = electron.Menu.buildFromTemplate([
