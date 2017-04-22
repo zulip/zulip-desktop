d18885ecc9cc5fccb0f27af3733b995025cb06d1
Zhongyi Tong <zhongyi.tong@foxmail.com>
2017-04-22 23:35:03 +0800

    Finish interactions of switching servers in ServerManagerView.

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -3,37 +3,104 @@
 const path = require("path");
 const DomainUtil = require(path.resolve(('app/renderer/js/utils/domain-util.js')));
 class ServerManagerView {
-    constructor() {
-        this.serverButtonTemplate = `
-            <div class="server-button active">
-                <div class="server-name" style="background-image: url();"></div>
-            </div>`;
-        this.$serversContainer = document.getElementById('servers-container');
+	constructor() {
+		this.$serversContainer = document.getElementById('servers-container');
 
-        const $actionsContainer = document.getElementById('actions-container');
-        this.$addServerButton = $actionsContainer.querySelector('#add-action');
-        this.$settingsButton = $actionsContainer.querySelector('#settings-action');
-    }
-    
-    init() {
-        this.domainUtil = new DomainUtil();
-        console.log(this.domainUtil.getDomains());
+		const $actionsContainer = document.getElementById('actions-container');
+		this.$addServerButton = $actionsContainer.querySelector('#add-action');
+		this.$settingsButton = $actionsContainer.querySelector('#settings-action');
+        this.$content = document.getElementById('content');
+
+        this.isLoading = false;
+	}
+
+	init() {
+		this.domainUtil = new DomainUtil();
+		this.initServers();
+	}
+
+	initServers() {
+		const servers = this.domainUtil.getDomains();
+		for (let server of servers) {
+			this.initServer(server);
+		}
+
+        const $firstServerButton = this.$serversContainer.firstChild;
+		$firstServerButton.classList.add('active');
+        this.$activeServerButton = $firstServerButton;
+        this.initWebView($firstServerButton.getAttribute('domain'));
+	}
+
+	initServer(server) {
+		const {
+			alias,
+			url
+		} = server;
+		const icon = server.icon || 'https://chat.zulip.org/static/images/logo/zulip-icon-128x128.271d0f6a0ca2.png';
+		const serverButtonTemplate = `
+                <div class="server-button" domain="${url}">
+                    <div class="server-name" style="background-image: url(${icon});"></div>
+                </div>`;
+		const $serverButton = this.__insert_node(serverButtonTemplate);
+		this.$serversContainer.appendChild($serverButton);
+		$serverButton.addEventListener('click', () => {
+            if (this.isLoading || this.$activeServerButton == $serverButton) return;
+
+            this.$activeServerButton.classList.remove('active');
+            $serverButton.classList.add('active');
+            const url = $serverButton.getAttribute('domain');
+            this.$activeServerButton = $serverButton;
+            this.startLoading(url);
+		});
+	}
+
+    initWebView(url) {
+        const webViewTemplate = `
+            <webview 
+                id="webview"
+                class="loading"
+                src="${url}" 
+                disablewebsecurity 
+                preload="js/preload.js"
+                webpreferences="allowRunningInsecureContent, javascript=yes">
+            </webview>
+        `;
+        this.$webView = this.__insert_node(webViewTemplate);
+		this.$content.appendChild(this.$webView);
+        this.$webView.addEventListener('dom-ready', this.endLoading.bind(this));
     }
 
-    initServers() {
+    startLoading(url) {
+        this.$webView.loadURL(url);
+        this.isLoading = true;
+        this.$webView.classList.add('loading');
+    }
+
+    endLoading() {
+        this.isLoading = false;
+        this.$webView.classList.remove('loading');
+    }
+
+	initActions() {
+
+	}
+
+	addServer() {
+
+	}
+
+    openSettings() {
         
     }
 
-    initActions() {
-
-    }
-
-    addServer() {
-
+    __insert_node(html) {
+        let wrapper= document.createElement('div');
+        wrapper.innerHTML= html;
+        return wrapper.firstElementChild;
     }
 }
 
 window.onload = () => {
-    const serverManagerView = new ServerManagerView();
-    serverManagerView.init();
+	const serverManagerView = new ServerManagerView();
+	serverManagerView.init();
 }
