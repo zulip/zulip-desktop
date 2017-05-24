777ed5c561eed0786437bf3985bdba7b8f105cfa
simplyahmazing <ahmad19526@gmail.com>
2017-05-23 22:11:22 -0400

    cleans up tests

--- tests/index.js
+++ tests/index.js
@@ -27,7 +27,7 @@ describe('application launch', function () {
       return this.app.stop()
     }
   })
-/*
+
   it('shows an initial window', function () {
      return this.app.client.waitUntilWindowLoaded(5000)
       .getWindowCount().should.eventually.equal(2)
@@ -38,30 +38,42 @@ describe('application launch', function () {
       .browserWindow.getBounds().should.eventually.have.property('width').and.be.above(0)
       .browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0)
   })
-*/
+
 	it('sets up a default organization', function () {
 		let app = this.app
+		let self = this
 		app.client.execute(() => {
 			window.confirm = function () { return true }
 		})
 
-		return this.app.client.waitUntilWindowLoaded(5000)
-			.pause(1000)
+		function createOrg (client, name, url, winIndex) {
+			return client
+				.then(switchToWebviewAtIndex.bind(null, self.app.client, winIndex))
+				.pause(1000)
+				.click('#new-server-action')
+				.setValue('input[id="server-info-name"]', name)
+				.setValue('input[id="server-info-url"]', url)
+				.click('#save-server-action')
+				.pause(500) // Need to pause while server verification takes place
+				.then(() =>  app.browserWindow.reload())
+			  .pause(1500)
+		}
+
+		function switchToWebviewAtIndex(client, index) {
+			return client
 			.windowHandles()
 			.then(function (session) {
-				console.log(session)
-				this.window(session.value[1])
+				this.window(session.value[index])
 			})
-			.click('#new-server-action')
-			.setValue('input[id="server-info-name"]', 'Zulip')
-			.setValue('input[id="server-info-url"]', 'chat.zulip.org')
-			.click('#save-server-action')
-			.pause(500) // Need to pause while server verification takes place
-			.then(() =>  app.browserWindow.reload())
-			.pause(1000)
-			.then(() => {
-				// app.browserWindow.focus()
-			})
+		}
+
+		return this.app.client.waitUntilWindowLoaded(5000)
+			.then(() => createOrg(self.app.client, 'Zulip 1', 'chat.zulip.org', 1))
+			.then(switchToWebviewAtIndex.bind(null, self.app.client, 0))
+			.click('#add-action > i')
+			.pause(500)
+			.then(switchToWebviewAtIndex.bind(null, self.app.client, 2))
+			.then(() => createOrg(self.app.client, 'Zulip 2', 'chat.zulip.org', 2))
 	})
 })
 
