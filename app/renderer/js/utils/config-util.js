81d3aa8a1b9dd4e36e19b070a02c00dbf7c444b9
Zhongyi Tong <zhongyi.tong@foxmail.com>
2017-07-03 01:43:07 +0800

    Add ConfigUtil to manage config items.

---
+++ app/renderer/js/utils/config-util.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const {app} = require('electron').remote;
+const JsonDB = require('node-json-db');
+
+let instance = null;
+
+class ConfigUtil {
+	constructor() {
+		if (instance) {
+			return instance;
+		} else {
+			instance = this;
+		}
+
+		this.db = new JsonDB(app.getPath('userData') + '/config.json', true, true);
+		return instance;
+	}
+
+	getConfigItem(key, defaultValue=null) {
+		let value = this.db.getData('/')[key];
+		if (value === undefined) {
+			this.setConfigItem(key, value);
+			return defaultValue;
+		} else {
+			return value;
+		}
+	}
+
+	setConfigItem(key, value) {
+		this.db.push(`/${key}`, value, true);
+	}
+
+	removeConfigItem(key) {
+		this.db.delete(`/${key}`);
+	}
+}
+
+module.exports = new ConfigUtil();
