a55cda3b1fc496b352db1e4647309dbcc9efe08c
akashnimare <svnitakash@gmail.com>
2017-07-06 19:23:35 +0530

    Added a warning dialog for deleting server

--- app/renderer/js/pages/preference/server-info-form.js
+++ app/renderer/js/pages/preference/server-info-form.js
@@ -1,4 +1,5 @@
 'use strict';
+const {dialog} = require('electron').remote;
 
 const BaseComponent = require(__dirname + '/../../components/base.js');
 const DomainUtil = require(__dirname + '/../../utils/domain-util.js');
@@ -53,8 +54,17 @@ class ServerInfoForm extends BaseComponent {
 
 	initActions() {
 		this.$deleteServerButton.addEventListener('click', () => {
-			DomainUtil.removeDomain(this.props.index);
-			this.props.onChange(this.props.index);
+			dialog.showMessageBox({
+				type: 'warning',
+				buttons: ['YES', 'NO'],
+				defaultId: 0,
+				message: 'Are you sure you want to delete this server?'
+			}, response => {
+				if (response === 0) {
+					DomainUtil.removeDomain(this.props.index);
+					this.props.onChange(this.props.index);
+				}
+			});
 		});
 	}
 }
