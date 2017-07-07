76cd62d0c8570af03be175dcb22575d1f44f25ef
akashnimare <svnitakash@gmail.com>
2017-07-08 04:43:16 +0530

    Added desktop notification support on windows 8 fixes #199

--- app/renderer/js/notification.js
+++ app/renderer/js/notification.js
@@ -1,6 +1,14 @@
 'use strict';
 
+const {remote} = require('electron');
+
 const ConfigUtil = require(__dirname + '/utils/config-util.js');
+
+const app = remote.app;
+
+// From https://github.com/felixrieseberg/electron-windows-notifications#appusermodelid
+// On windows 8 we have to explicitly set the appUserModelId otherwise notification won't work.
+app.setAppUserModelId('org.zulip.zulip-electron');
 
 const NativeNotification = window.Notification;
 
