afd24035f43328fe2bde35c5853eaca769c64fb2
akashnimare <svnitakash@gmail.com>
2017-08-29 03:28:07 +0530

    Don't run start at login script in dev mode

--- app/main/startup.js
+++ app/main/startup.js
@@ -1,9 +1,15 @@
 'use strict';
 const { app } = require('electron');
 const AutoLaunch = require('auto-launch');
+const isDev = require('electron-is-dev');
 const ConfigUtil = require('./../renderer/js/utils/config-util.js');
 
 const setAutoLaunch = AutoLaunchValue => {
+	// Don't run this in development
+	if (isDev) {
+		return;
+	}
+
 	// On Mac, work around a bug in auto-launch where it opens a Terminal window
 	// See https://github.com/Teamwork/node-auto-launch/issues/28#issuecomment-222194437
 
