0dc97648a0e7991105ab08c6af807dd719c7aa8a
akashnimare <svnitakash@gmail.com>
2017-09-06 03:54:25 +0530

    code refactor

--- app/renderer/js/pages/preference/general-section.js
+++ app/renderer/js/pages/preference/general-section.js
@@ -153,13 +153,13 @@ class GeneralSection extends BaseSection {
 		});
 	}
 
-	clearAppData() {
+	clearAppDataDialog() {
 		const clearAppDataMessage = 'By clicking proceed you will be removing all added accounts and preferences from Zulip. When the application restarts, it will be as if you are starting Zulip for the first time.';
 		const getAppPath = path.join(app.getPath('appData'), app.getName());
 
 		dialog.showMessageBox({
 			type: 'warning',
-			buttons: ['Yes', 'No'],
+			buttons: ['YES', 'NO'],
 			defaultId: 0,
 			message: 'Are you sure',
 			detail: clearAppDataMessage
@@ -174,7 +174,7 @@ class GeneralSection extends BaseSection {
 	updateResetDataOption() {
 		const resetDataButton = document.querySelector('#resetdata-option .reset-data-button');
 		resetDataButton.addEventListener('click', () => {
-			this.clearAppData();
+			this.clearAppDataDialog();
 		});
 	}
 
