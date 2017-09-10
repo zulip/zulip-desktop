29e347c5116d3872389eb4667d4d4837a45f9040
Cedricium <amaya.cedric@gmail.com>
2017-09-10 14:07:04 -0700

    List application-specific shortcuts only

--- app/renderer/js/pages/preference/shortcuts-section.js
+++ app/renderer/js/pages/preference/shortcuts-section.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { shell } = require('electron');
-
 const BaseSection = require(__dirname + '/base-section.js');
 
 class ShortcutsSection extends BaseSection {
@@ -10,276 +8,294 @@ class ShortcutsSection extends BaseSection {
 		this.props = props;
 	}
 
-	template() {
-		let userOSKey = 'Ctrl';
-		if (process.platform === 'darwin') {
-			userOSKey = '⌘';
-		}
+	templateMac() {
+		const userOSKey = '⌘';
 
 		return `
             <div class="settings-pane">
-              <div class="title">Desktop-specific</div>
+              <div class="title">Application Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>${userOSKey}</kbd> + <kbd>,</kbd></td>
-                    <td>Manage servers</td>
+                    <td><kbd>${userOSKey}</kbd><kbd>,</kbd></td>
+                    <td>Settings</td>
                   </tr>
                   <tr>
-                    <td><kbd>${userOSKey}</kbd> + <kbd>[</kbd></td>
+                    <td><kbd>${userOSKey}</kbd><kbd>K</kbd></td>
+                    <td>Keyboard Shortcuts</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>D</kbd></td>
+                    <td>Reset App Settings</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>L</kbd></td>
+                    <td>Log Out</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>H</kbd></td>
+                    <td>Hide Zulip</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Option</kbd><kbd>${userOSKey}</kbd><kbd>H</kbd></td>
+                    <td>Hide Others</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>Q</kbd></td>
+                    <td>Quit Zulip</td>
+                  </tr>
+                </table>
+                <div class="setting-control"></div>
+              </div>
+              <div class="title">Edit Shortcuts</div>
+              <div class="settings-card">
+                <table>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>Z</kbd></td>
+                    <td>Undo</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>Z</kbd></td>
+                    <td>Redo</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>X</kbd></td>
+                    <td>Cut</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>C</kbd></td>
+                    <td>Copy</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>V</kbd></td>
+                    <td>Paste</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>V</kbd></td>
+                    <td>Paste and Match Style</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>A</kbd></td>
+                    <td>Select All</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>F</kbd></td>
+                    <td>Find</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>G</kbd></td>
+                    <td>Find Next</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Control</kbd><kbd>${userOSKey}</kbd><kbd>Space</kbd></td>
+                    <td>Emoji & Symbols</td>
+                  </tr>
+                </table>
+                <div class="setting-control"></div>
+              </div>
+              <div class="title">View Shortcuts</div>
+              <div class="settings-card">
+                <table>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>R</kbd></td>
+                    <td>Reload</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>R</kbd></td>
+                    <td>Hard Reload</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Control</kbd><kbd>${userOSKey}</kbd><kbd>F</kbd></td>
+                    <td>Enter Full Screen</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>=</kbd></td>
+                    <td>Zoom In</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>-</kbd></td>
+                    <td>Zoom Out</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>0</kbd></td>
+                    <td>Actual Size</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>S</kbd></td>
+                    <td>Toggle Sidebar</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Option</kbd><kbd>${userOSKey}</kbd><kbd>I</kbd></td>
+                    <td>Toggle DevTools for Zulip App</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>Option</kbd><kbd>${userOSKey}</kbd><kbd>U</kbd></td>
+                    <td>Toggle DevTools for Active Tab</td>
+                  </tr>
+                </table>
+                <div class="setting-control"></div>
+              </div>
+              <div class="title">History Shortcuts</div>
+              <div class="settings-card">
+                <table>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd><kbd>←</kbd></td>
                     <td>Back</td>
                   </tr>
                   <tr>
-                    <td><kbd>${userOSKey}</kbd> + <kbd>]</kbd></td>
+                    <td><kbd>${userOSKey}</kbd><kbd>→</kbd></td>
                     <td>Forward</td>
                   </tr>
                 </table>
                 <div class="setting-control"></div>
               </div>
-              <div class="title">Navigation</div>
+              <div class="title">Window Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>/</kbd></td>
-                    <td>Initiate a search</td>
+                    <td><kbd>${userOSKey}</kbd><kbd>M</kbd></td>
+                    <td>Minimize</td>
                   </tr>
                   <tr>
-                    <td><kbd>q</kbd></td>
-                    <td>Search people</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>w</kbd></td>
-                    <td>Search streams</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>↑</kbd>, <kbd>k</kbd></td>
-                    <td>Previous message</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>↓</kbd>, <kbd>j</kbd></td>
-                    <td>Next message</td>
-                  </tr>
-									<tr>
-                    <td><kbd>PgUp</kbd>, <kbd>K</kbd></td>
-                    <td>Scroll up</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>PgDn</kbd>, <kbd>Space</kbd>, <kbd>J</kbd></td>
-                    <td>Scroll down</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>End</kbd>, <kbd>G</kbd></td>
-                    <td>Last message</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>Home</kbd></td>
-                    <td>First message</td>
+                    <td><kbd>${userOSKey}</kbd><kbd>W</kbd></td>
+                    <td>Close</td>
                   </tr>
                 </table>
                 <div class="setting-control"></div>
               </div>
-              <div class="title">Composing Messages</div>
+            </div>
+		`;
+	}
+
+	templateWinLin() {
+		const userOSKey = 'Ctrl';
+
+		return `
+            <div class="settings-pane">
+              <div class="title">Application Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>Enter</kbd>, <kbd>r</kbd></td>
-                    <td>Reply to message</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>,</kbd></td>
+                    <td>Settings</td>
                   </tr>
                   <tr>
-                    <td><kbd>R</kbd></td>
-                    <td>Reply to author</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>K</kbd></td>
+                    <td>Keyboard Shortcuts</td>
                   </tr>
                   <tr>
-                    <td><kbd>c</kbd></td>
-                    <td>New stream message</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>L</kbd></td>
+                    <td>Log Out</td>
                   </tr>
                   <tr>
-                    <td><kbd>C</kbd></td>
-                    <td>New private message</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>@</kbd></td>
-                    <td>Compose a reply @-mentioning author</td>
-                  </tr>
-									<tr>
-                    <td><kbd>PgUp</kbd>, <kbd>K</kbd></td>
-                    <td>Scroll up</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>${userOSKey}</kbd> + <kbd>Enter</kbd></td>
-                    <td>Send message</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>Shift</kbd> + <kbd>Enter</kbd></td>
-                    <td>Insert new line</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>Esc</kbd>, <kbd>Ctrl</kbd> + <kbd>[</kbd></td>
-                    <td>Cancel compose</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Q</kbd></td>
+                    <td>Quit Zulip</td>
                   </tr>
                 </table>
                 <div class="setting-control"></div>
               </div>
-							<div class="title">Message Actions</div>
+              <div class="title">Edit Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>←</kbd></td>
-                    <td>Edit your last message</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Z</kbd></td>
+                    <td>Undo</td>
                   </tr>
                   <tr>
-                    <td><kbd>u</kbd></td>
-                    <td>Show message sender's profile</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Y</kbd></td>
+                    <td>Redo</td>
                   </tr>
                   <tr>
-                    <td><kbd>v</kbd></td>
-                    <td>Show images in thread</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>X</kbd></td>
+                    <td>Cut</td>
                   </tr>
                   <tr>
-                    <td><kbd>*</kbd></td>
-                    <td>Star selected message</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>C</kbd></td>
+                    <td>Copy</td>
                   </tr>
                   <tr>
-                    <td><kbd>+</kbd></td>
-                    <td>React to selected message with 👍</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>V</kbd></td>
+                    <td>Paste</td>
                   </tr>
                   <tr>
-                    <td><kbd>-</kbd></td>
-                    <td>Collapse/show selected message</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>V</kbd></td>
+                    <td>Paste and Match Style</td>
                   </tr>
                   <tr>
-                    <td><kbd>M</kbd></td>
-                    <td>Toggle topic mute</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>A</kbd></td>
+                    <td>Select All</td>
                   </tr>
                 </table>
                 <div class="setting-control"></div>
               </div>
-							<div class="title">Narrowing</div>
+              <div class="title">View Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>s</kbd></td>
-                    <td>Narrow by stream</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>R</kbd></td>
+                    <td>Reload</td>
                   </tr>
                   <tr>
-                    <td><kbd>S</kbd></td>
-                    <td>Narrow by topic</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>R</kbd></td>
+                    <td>Hard Reload</td>
                   </tr>
                   <tr>
-                    <td><kbd>P</kbd></td>
-                    <td>Narrow to all private messages</td>
+                    <td><kbd>F11</kbd></td>
+                    <td>Toggle Full Screen</td>
                   </tr>
                   <tr>
-                    <td><kbd>n</kbd></td>
-                    <td>Narrow to next unread topic</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>=</kbd></td>
+                    <td>Zoom In</td>
                   </tr>
                   <tr>
-                    <td><kbd>A</kbd>, <kbd>D</kbd></td>
-                    <td>Cycle between stream narrows</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>-</kbd></td>
+                    <td>Zoom Out</td>
                   </tr>
                   <tr>
-                    <td><kbd>Esc</kbd>, <kbd>${userOSKey}</kbd> + <kbd>[</kbd></td>
-                    <td>Return to Home view</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>0</kbd></td>
+                    <td>Actual Size</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>S</kbd></td>
+                    <td>Toggle Sidebar</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>I</kbd></td>
+                    <td>Toggle DevTools for Zulip App</td>
+                  </tr>
+                  <tr>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>U</kbd></td>
+                    <td>Toggle DevTools for Active Tab</td>
                   </tr>
                 </table>
                 <div class="setting-control"></div>
               </div>
-							<div class="title">Menus</div>
+              <div class="title">History Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>g</kbd></td>
-                    <td>Toggle the gear menu</td>
+                    <td><kbd>Alt</kbd> + <kbd>←</kbd></td>
+                    <td>Back</td>
                   </tr>
                   <tr>
-                    <td><kbd>i</kbd></td>
-                    <td>Open message menu</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>:</kbd></td>
-                    <td>Open reactions menu</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>?</kbd></td>
-                    <td>Show keyboard shortcuts</td>
+                    <td><kbd>Alt</kbd> + <kbd>→</kbd></td>
+                    <td>Forward</td>
                   </tr>
                 </table>
                 <div class="setting-control"></div>
               </div>
-							<div class="title">Drafts</div>
+              <div class="title">Window Shortcuts</div>
               <div class="settings-card">
                 <table>
                   <tr>
-                    <td><kbd>d</kbd></td>
-                    <td>View drafts</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>M</kbd></td>
+                    <td>Minimize</td>
                   </tr>
                   <tr>
-                    <td><kbd>↑</kbd></td>
-                    <td>Select previous draft</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>↓</kbd></td>
-                    <td>Select next draft</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>PgUp</kbd> (<kbd>Fn</kbd> + <kbd>↑</kbd>)</td>
-                    <td>Scroll up</td>
-                  </tr>
-									<tr>
-                    <td><kbd>Space</kbd>, <kbd>PgDn</kbd> (<kbd>Fn</kbd> + <kbd>↓</kbd>)</td>
-                    <td>Scroll down</td>
-                  </tr>
-									<tr>
-                    <td><kbd>Home</kbd> (<kbd>Fn</kbd> + <kbd>←</kbd>)</td>
-                    <td>Select first draft</td>
-                  </tr>
-									<tr>
-                    <td><kbd>End</kbd> (<kbd>Fn</kbd> + <kbd>→</kbd>), <kbd>G</kbd></td>
-                    <td>Select last draft</td>
-                  </tr>
-									<tr>
-                    <td><kbd>Enter</kbd> (<kbd>Return</kbd>)</td>
-                    <td>Edit selected draft</td>
-                  </tr>
-									<tr>
-                    <td><kbd>Backspace</kbd> (<kbd>Delete</kbd>)</td>
-                    <td>Delete selected draft</td>
+                    <td><kbd>${userOSKey}</kbd> + <kbd>W</kbd></td>
+                    <td>Close</td>
                   </tr>
                 </table>
-                <div class="setting-control"></div>
-              </div>
-							<div class="title">Streams</div>
-              <div class="settings-card">
-                <table>
-                  <tr>
-                    <td><kbd>↑</kbd> and <kbd>↓</kbd></td>
-                    <td>Scroll through streams</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>←</kbd> and <kbd>→</kbd></td>
-                    <td>Switch between tabs</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>V</kbd></td>
-                    <td>View stream messages</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>S</kbd></td>
-                    <td>Subscribe/unsubscribe from selected stream</td>
-                  </tr>
-                  <tr>
-                    <td><kbd>n</kbd></td>
-                    <td>Create new stream</td>
-                  </tr>
-                </table>
-                <div class="setting-control"></div>
-              </div>
-							<div class="title">Documentation</div>
-              <div class="settings-card">
-								<span id="open-shortcuts-url">
-									Detailed keyboard shortcuts documentation found here
-									<i class="material-icons open-tab-button">open_in_new</i>
-								</span>
                 <div class="setting-control"></div>
               </div>
             </div>
@@ -287,15 +303,8 @@ class ShortcutsSection extends BaseSection {
 	}
 
 	init() {
-		this.props.$root.innerHTML = this.template();
-		this.openExternalShortcutsLink();
-	}
-
-	openExternalShortcutsLink() {
-		const externalShortcutsLink = document.getElementById('open-shortcuts-url');
-		externalShortcutsLink.addEventListener('click', () => {
-			shell.openExternal('https://chat.zulip.org/help/keyboard-shortcuts');
-		});
+		this.props.$root.innerHTML = (process.platform === 'darwin') ?
+			this.templateMac() : this.templateWinLin();
 	}
 }
 
