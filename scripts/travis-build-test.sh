c9e022b367e59039a767695f535ca3b98d2f8fb8
simplyahmazing <ahmad19526@gmail.com>
2017-10-12 10:10:34 -0400

    add xrandr extensions to Xvfb

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -3,7 +3,7 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export {no_proxy,NO_PROXY}="127.0.0.1,localhost"
 	export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
+  sh -e /etc/init.d/xvfb +extension RANDR start
   sleep 3
 fi
 
