371f412b66c53e920ca94fa613ebc0c20091730a
simplyahmazing <ahmad19526@gmail.com>
2017-10-12 10:42:41 -0400

    call xvfb init after start-stop-daemon on travis

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-#   export {no_proxy,NO_PROXY}="127.0.0.1,localhost"
-# 	export DISPLAY=:99.0
-#   sh -e /etc/init.d/xvfb +extension RANDR start
-#   sleep 3
-# fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  export {no_proxy,NO_PROXY}="127.0.0.1,localhost"
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+fi
 
 echo 'Screen Resolution:'
 xdpyinfo | grep dimensions
