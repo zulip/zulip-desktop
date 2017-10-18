266370e274d6219fbb9e7a1df42c04a8088d97c4
simplyahmazing <ahmad19526@gmail.com>
2017-10-17 21:52:10 -0400

    fix formatting

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export {no_proxy,NO_PROXY}="127.0.0.1,localhost"
-  export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
-  sleep 3
+    export {no_proxy,NO_PROXY}="127.0.0.1,localhost"
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3
+
+    echo 'Travis Screen Resolution:'
+    xdpyinfo | grep dimensions
 fi
-
-echo 'Screen Resolution:'
-xdpyinfo | grep dimensions
-
-
-echo 'Screen Resolution (xrandr):'
-xrandr | fgrep '*'
 
 npm run test
