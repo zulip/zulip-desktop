e290ded72e2a35ebe9056a6d6dc3bf77ab36425a
simplyahmazing <ahmad19526@gmail.com>
2017-10-18 15:04:52 -0400

    remove unused TEST_DIR config

--- tests/config.js
+++ tests/config.js
@@ -1,10 +1,7 @@
 const path = require('path')
 
-const TEMP_DIR = process.platform === 'win32' ? 'C:\\Windows\\Temp' : '/tmp'
-const TEST_DIR = path.join(TEMP_DIR, 'ZulipTests')
 const TEST_APP_PRODUCT_NAME = 'ZulipTest'
 
 module.exports = {
-  TEST_DIR,
   TEST_APP_PRODUCT_NAME
 }
