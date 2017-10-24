05dc69f6f8f68b9d49743e22a31be3e2ffb489d9
simplyahmazing <ahmad19526@gmail.com>
2017-10-23 23:28:19 -0400

    start xvfb from script on ci

---
+++ scripts/travis-xvfb.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+fi
