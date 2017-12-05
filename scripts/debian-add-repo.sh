a1d5a35ccfd1f930eee38d565384939355345886
akashnimare <svnitakash@gmail.com>
2017-12-05 21:01:26 +0530

    debian: Remove config files when user uninstalls app

--- scripts/debian-add-repo.sh
+++ scripts/debian-add-repo.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# This script runs when user install the debian package
+
 # Install apt repository source list if it does not exist
-if ! grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep zulip; then
-  sudo apt-key adv --keyserver pool.sks-keyservers.net --recv 69AD12704E71A4803DCA3A682424BE5AE9BD10D9
-  echo "deb https://dl.bintray.com/zulip/debian/ stable main" | \
-  sudo tee -a /etc/apt/sources.list.d/zulip.list
+if ! grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep zulip.list; then
+    sudo apt-key adv --keyserver pool.sks-keyservers.net --recv 69AD12704E71A4803DCA3A682424BE5AE9BD10D9
+    echo "deb https://dl.bintray.com/zulip/debian/ stable main" | \
+    sudo tee -a /etc/apt/sources.list.d/zulip.list;
 fi
