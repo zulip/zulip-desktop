6b51f6d9a6df015646dea20605647bc2bc6a4ab9
simplyahmazing <ahmad19526@gmail.com>
2017-12-11 16:34:23 -0500

    enable spellchecker via config for test

--- tests/unit/test-spellchecker.js
+++ tests/unit/test-spellchecker.js
@@ -1,8 +1,12 @@
+const ConfigUtil = require('js/utils/config-util.js');
 const SetupSpellChecker = require('js/spellchecker')
 
 describe('test spell checker', function () {
-  SetupSpellChecker.init(true)
+  ConfigUtil.setConfigItem('enableSpellchecker', true);
+  SetupSpellChecker.init()  // re-initialize after setting update
+
   const spellCheckHandler = SetupSpellChecker.SpellCheckHandler
+
   it('mark misspelled word', function () {
     expect(spellCheckHandler.isMisspelled('helpe')).toBe(true)
   })
