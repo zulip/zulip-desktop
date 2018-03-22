ea6665cd108a106e3afb884d67ca2a99fc85dccf
Akash Nimare <svnitakash@gmail.com>
2018-03-22 14:15:19 +0530

    security: Code clean up.

--- app/renderer/js/shared/preventdrag.js
+++ app/renderer/js/shared/preventdrag.js
@@ -1,14 +1,17 @@
 'use strict';
 
-const preventDragandDrop = () => {
+// This is a security fix. Following function prevents drag and drop event in the app
+// so that attackers can't execute any remote code within the app
+// It doesn't affect the compose box so that users can still
+// use drag and drop event to share files etc
+
+const preventDragAndDrop = () => {
 	document.addEventListener('dragover', event => {
-		console.log(event);
 		event.preventDefault();
 	});
 	document.addEventListener('drop', event => {
-		console.log(event);
 		event.preventDefault();
 	});
 };
 
-preventDragandDrop();
+preventDragAndDrop();
