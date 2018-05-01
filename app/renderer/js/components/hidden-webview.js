5638590c8b2bfd84f7d4b23ff5f83be34ffbb3a6
Abhigyan Khaund <mail@abhigyan.xyz>
2018-05-01 20:37:33 +0530

    internal-links: Download file attachment links using downloadURL.
    This commit download file attachments using downloadURL method of
    webContent, the same way loadURL opens internal links. This
    removes the use of hidden webview added in
    f70432f4e3cdc86ae610bf3d77bfa2e784fabdf3.
    
    Improves: #469.

--- app/renderer/js/components/hidden-webview.js
+++
@@ -1,9 +0,0 @@
-// this hidden webview will be used to open pdf url and
-// save it to user's computer without triggering a reload
-// when navigating to pdf url to download it.
-const hiddenWebView = document.createElement('webview');
-hiddenWebView.classList.add('download-webview');
-hiddenWebView.src = 'about:blank';
-document.querySelector('#content').appendChild(hiddenWebView);
-
-module.exports = hiddenWebView;
