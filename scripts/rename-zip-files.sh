47b90e103e8011f48cfbcdf52dd00e844302f04e
Priyank Patel <priyankp390@gmail.com>
2018-05-07 11:35:25 -0400

    squash: try fixing script

--- scripts/rename-zip-files.sh
+++ scripts/rename-zip-files.sh
@@ -3,7 +3,12 @@
 
 set -e
 set -x
- 
+
+# make a zips directory to hold all the renamed files
+# we move them out when every zip file for each file is renamed
+mkdir -p dist/zips
+
 ZIP_BUILD_FILE=`ls dist/*.zip`
 echo ZIP_BUILD_FILE
 mv ZIP_BUILD_FILE `node -pe "'$ZIP_BUILD_FILE'.replace('.zip', '-$1.zip')"`
+mv dist/*.zip dist/zips/
