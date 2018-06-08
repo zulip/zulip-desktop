8ecd7d74dd806a8a0e33e094c63d3d5e28136d3f
Abhigyan Khaund <mail@abhigyan.xyz>
2018-06-09 02:25:40 +0530

    proxy-setting: Use all async resolveProxy function with promises.

--- app/renderer/js/utils/proxy-util.js
+++ app/renderer/js/utils/proxy-util.js
@@ -20,49 +20,75 @@ class ProxyUtil {
 		const ses = page.session;
 		const resolveProxyUrl = 'www.google.com';
 
-		let proxyString = '';
-
 		// Check HTTP Proxy
-		ses.resolveProxy('http://' + resolveProxyUrl, proxy => {
-			if (proxy !== 'DIRECT') {
-				// in case of proxy HTTPS url:port, windows gives first word as HTTPS while linux gives PROXY
-				// for all other HTTP or direct url:port both uses PROXY
-				if (proxy.includes('PROXY') || proxy.includes('HTTPS')) {
-					proxyString += 'http=' + proxy.split('PROXY')[1] + ';';
+		const httpProxy = new Promise(resolve => {
+			ses.resolveProxy('http://' + resolveProxyUrl, proxy => {
+				let httpString = '';
+				if (proxy !== 'DIRECT') {
+					// in case of proxy HTTPS url:port, windows gives first word as HTTPS while linux gives PROXY
+					// for all other HTTP or direct url:port both uses PROXY
+					if (proxy.includes('PROXY') || proxy.includes('HTTPS')) {
+						httpString = 'http=' + proxy.split('PROXY')[1] + ';';
+					}
 				}
-			}
+				resolve(httpString);
+			});
 		});
 		// Check HTTPS Proxy
-		ses.resolveProxy('https://' + resolveProxyUrl, proxy => {
-			if (proxy !== 'DIRECT' || proxy.includes('HTTPS')) {
-				// in case of proxy HTTPS url:port, windows gives first word as HTTPS while linux gives PROXY
-				// for all other HTTP or direct url:port both uses PROXY
-				if (proxy.includes('PROXY' || proxy.includes('HTTPS'))) {
-					proxyString += 'https=' + proxy.split('PROXY')[1] + ';';
+		const httpsProxy = new Promise(resolve => {
+			ses.resolveProxy('https://' + resolveProxyUrl, proxy => {
+				let httpsString = '';
+				if (proxy !== 'DIRECT' || proxy.includes('HTTPS')) {
+					// in case of proxy HTTPS url:port, windows gives first word as HTTPS while linux gives PROXY
+					// for all other HTTP or direct url:port both uses PROXY
+					if (proxy.includes('PROXY' || proxy.includes('HTTPS'))) {
+						httpsString += 'https=' + proxy.split('PROXY')[1] + ';';
+					}
 				}
-			}
+				resolve(httpsString);
+			});
 		});
 
 		// Check FTP Proxy
-		ses.resolveProxy('ftp://' + resolveProxyUrl, proxy => {
-			if (proxy !== 'DIRECT') {
-				if (proxy.includes('PROXY')) {
-					proxyString += 'ftp=' + proxy.split('PROXY')[1] + ';';
+		const ftpProxy = new Promise(resolve => {
+			ses.resolveProxy('ftp://' + resolveProxyUrl, proxy => {
+				let ftpString = '';
+				if (proxy !== 'DIRECT') {
+					if (proxy.includes('PROXY')) {
+						ftpString += 'ftp=' + proxy.split('PROXY')[1] + ';';
+					}
 				}
-			}
+				resolve(ftpString);
+			});
 		});
 
 		// Check SOCKS Proxy
-		ses.resolveProxy('socks4://' + resolveProxyUrl, proxy => {
-			if (proxy !== 'DIRECT') {
-				if (proxy.includes('SOCKS5')) {
-					proxyString += 'socks=' + proxy.split('SOCKS5')[1] + ';';
-				} else if (proxy.includes('SOCKS4')) {
-					proxyString += 'socks=' + proxy.split('SOCKS4')[1] + ';';
+		const socksProxy = new Promise(resolve => {
+			ses.resolveProxy('socks4://' + resolveProxyUrl, proxy => {
+				let socksString = '';
+				if (proxy !== 'DIRECT') {
+					if (proxy.includes('SOCKS5')) {
+						socksString += 'socks=' + proxy.split('SOCKS5')[1] + ';';
+					} else if (proxy.includes('SOCKS4')) {
+						socksString += 'socks=' + proxy.split('SOCKS4')[1] + ';';
+					} else if (proxy.includes('PROXY')) {
+						socksString += 'socks=' + proxy.split('PROXY')[1] + ';';
+					}
 				}
-			}
-			// proxyString is written here so that empty string is not written as resolveProxy is async
+				resolve(socksString);
+			});
+		});
+
+		Promise.all([httpProxy, httpsProxy, ftpProxy, socksProxy]).then(values => {
+			let proxyString = '';
+			values.forEach(proxy => {
+				proxyString += proxy;
+			});
 			ConfigUtil.setConfigItem('systemProxyRules', proxyString);
+			const useSystemProxy = ConfigUtil.getConfigItem('useSystemProxy');
+			if (useSystemProxy) {
+				ConfigUtil.setConfigItem('proxyRules', proxyString);
+			}
 		});
 	}
 }
