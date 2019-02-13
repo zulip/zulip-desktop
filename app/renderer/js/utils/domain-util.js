15db35d2d26002365c4ca4969658f0bc3e2fb976
Swapnil Rustagi <swapnilrustagi97792@gmail.com>
2019-02-14 03:00:18 +0530

    Check for zulip.ogg only if server error is 404, for old Zulip servers. Fixes issue 573

--- app/renderer/js/utils/domain-util.js
+++ app/renderer/js/utils/domain-util.js
@@ -127,7 +127,7 @@ class DomainUtil {
 
 		// If certificate for the domain exists add it as a ca key in the request's parameter else consider only domain as the parameter for request
 		// Add proxy as a parameter if it sbeing used.
-		const checkDomain = {
+		const checkDomainFallback = {
 			url: domain + '/static/audio/zulip.ogg',
 			ca: (certificateLocation) ? certificateLocation : '',
 			proxy: proxyEnabled ? ProxyUtil.getProxy(domain) : '',
@@ -135,6 +135,13 @@ class DomainUtil {
 			headers: { 'User-Agent': SystemUtil.getUserAgent() }
 		};
 
+		const checkDomain = {
+			url: domain + '/api/v1/server_settings',
+			ca: (certificateLocation) ? certificateLocation : '',
+			proxy: proxyEnabled ? ProxyUtil.getProxy(domain) : '',
+			ecdhCurve: 'auto',
+			headers: { 'User-Agent': SystemUtil.getUserAgent() }
+		};
 		const serverConf = {
 			icon: defaultIconUrl,
 			url: domain,
@@ -160,41 +167,50 @@ class DomainUtil {
 					}, () => {
 						resolve(serverConf);
 					});
-				} else if (domain.indexOf(whitelistDomains) >= 0 || certsError) {
-					if (silent) {
-						this.getServerSettings(domain).then(serverSettings => {
-							resolve(serverSettings);
-						}, () => {
-							resolve(serverConf);
-						});
-					} else {
-						// Report error to sentry to get idea of possible certificate errors
-						// users get when adding the servers
-						logger.reportSentry(new Error(error));
-						const certErrorMessage = `Do you trust certificate from ${domain}? \n ${error}`;
-						const certErrorDetail = `The organization you're connecting to is either someone impersonating the Zulip server you entered, or the server you're trying to connect to is configured in an insecure way.
-						\nIf you have a valid certificate please add it from Settings>Organizations and try to add the organization again.
-						\nUnless you have a good reason to believe otherwise, you should not proceed.
-						\nYou can click here if you'd like to proceed with the connection.`;
-
-						dialog.showMessageBox({
-							type: 'warning',
-							buttons: ['Yes', 'No'],
-							defaultId: 1,
-							message: certErrorMessage,
-							detail: certErrorDetail
-						}, response => {
-							if (response === 0) {
+				} else if (!error && response.statusCode === 404) {
+					// Old Zulip server
+					request(checkDomainFallback, errorFall => {
+						if (domain.indexOf(whitelistDomains) >= 0 || certsError) {
+							if (silent) {
 								this.getServerSettings(domain).then(serverSettings => {
 									resolve(serverSettings);
 								}, () => {
 									resolve(serverConf);
 								});
 							} else {
-								reject('Untrusted Certificate.');
+								// Report error to sentry to get idea of possible certificate errors
+								// users get when adding the servers
+								logger.reportSentry(new Error(errorFall));
+								const certErrorMessage = `Do you trust certificate from ${domain}? \n ${error}`;
+								const certErrorDetail = `The organization you're connecting to is either someone impersonating the Zulip server you entered, or the server you're trying to connect to is configured in an insecure way.
+								\nIf you have a valid certificate please add it from Settings>Organizations and try to add the organization again.
+								\nUnless you have a good reason to believe otherwise, you should not proceed.
+								\nYou can click here if you'd like to proceed with the connection.`;
+								dialog.showMessageBox({
+									type: 'warning',
+									buttons: ['Yes', 'No'],
+									defaultId: 1,
+									message: certErrorMessage,
+									detail: certErrorDetail
+								}, response => {
+									if (response === 0) {
+										this.getServerSettings(domain).then(serverSettings => {
+											resolve(serverSettings);
+										}, () => {
+											resolve(serverConf);
+										});
+									} else {
+										reject('Untrusted Certificate.');
+									}
+								});
 							}
-						});
-					}
+						} else {
+							const invalidZulipServerError = `${domain} does not appear to be a valid Zulip server. Make sure that \
+							\n (1) you can connect to that URL in a web browser and \n (2) if you need a proxy to connect to the Internet, that you've configured your proxy in the Network settings \n (3) its a zulip server \
+							\n (4) the server has a valid certificate, you can add custom certificates in Settings>Organizations`;
+							reject(invalidZulipServerError);
+						}
+					});
 				} else {
 					const invalidZulipServerError = `${domain} does not appear to be a valid Zulip server. Make sure that \
 					\n (1) you can connect to that URL in a web browser and \n (2) if you need a proxy to connect to the Internet, that you've configured your proxy in the Network settings \n (3) its a zulip server \
