748be34ad00b050bd4aa8fd5a01ac86c5a14c303
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-03-25 01:00:00 +0530

    Trim domain to first word in server URL
    If the app is unable to fetch the realm name, the server alias will now
    default to first word after the protocol scheme in the server URL.

--- app/renderer/js/utils/domain-util.js
+++ app/renderer/js/utils/domain-util.js
@@ -104,6 +104,12 @@ class DomainUtil {
 		return false;
 	}
 
+	trimDomain(domain) {
+		let subDomain = domain.substring(domain.lastIndexOf('://') + 3);
+		subDomain = subDomain.substring(0, subDomain.indexOf('.'));
+		return subDomain;
+	}
+
 	// ignoreCerts parameter helps in fetching server icon and
 	// other server details when user chooses to ignore certificate warnings
 	checkDomain(domain, ignoreCerts = false, silent = false) {
@@ -142,7 +148,7 @@ class DomainUtil {
 		const serverConf = {
 			icon: defaultIconUrl,
 			url: domain,
-			alias: domain,
+			alias: this.trimDomain(domain),
 			ignoreCerts
 		};
 
