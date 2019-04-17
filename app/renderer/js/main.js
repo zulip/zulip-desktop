9d6ac2720b7aa3adc05deb185eaa6dbbe8e5a3f9
Abhigyan Khaund <20434085+abhigyank@users.noreply.github.com>
2019-04-17 09:38:00 +0530

    Add "role" key to webview property.

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -186,6 +186,7 @@ class ServerManagerView {
 				index,
 				tabIndex,
 				url: server.url,
+				role: 'server',
 				name: CommonUtil.decodeString(server.alias),
 				isActive: () => {
 					return index === this.activeTabIndex;
@@ -337,6 +338,7 @@ class ServerManagerView {
 				index: this.functionalTabs[tabProps.name],
 				tabIndex,
 				url: tabProps.url,
+				role: 'function',
 				name: tabProps.name,
 				isActive: () => {
 					return this.functionalTabs[tabProps.name] === this.activeTabIndex;
