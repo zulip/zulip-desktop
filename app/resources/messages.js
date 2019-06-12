31b00ee6a60b3ffe7a4f0aef4135b6f51ea0fa73
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-06-13 00:26:32 +0530

    Update server validation logic.
    This PR removes .ogg file check (supported only by very old servers). Other enhancements in server validation logic -
    * Reject domains with no organizations. 
    * Convert validation methods to async await
    * Add messages.js for returning error message strings.
    
    Fixes: #596, #573.

---
+++ app/resources/messages.js
@@ -0,0 +1,27 @@
+class Messages {
+	invalidZulipServerError(domain) {
+		return `${domain} does not appear to be a valid Zulip server. Make sure that
+			\n • You can connect to that URL in a web browser.\
+			\n • If you need a proxy to connect to the Internet, that you've configured your proxy in the Network settings.\
+			\n • It's a Zulip server. (The oldest supported version is 1.6).\
+			\n • The server has a valid certificate. (You can add custom certificates in Settings > Organizations).`;
+	}
+
+	noOrgsError(domain) {
+		return `${domain} does not have any organizations added.\
+		\nPlease contact your server administrator.`;
+	}
+
+	certErrorMessage(domain, error) {
+		return `Do you trust certificate from ${domain}? \n ${error}`;
+	}
+
+	certErrorDetail() {
+		return `The organization you're connecting to is either someone impersonating the Zulip server you entered, or the server you're trying to connect to is configured in an insecure way.
+		\nIf you have a valid certificate please add it from Settings>Organizations and try to add the organization again.
+		\nUnless you have a good reason to believe otherwise, you should not proceed.
+		\nYou can click here if you'd like to proceed with the connection.`;
+	}
+}
+
+module.exports = new Messages();
