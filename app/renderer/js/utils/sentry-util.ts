29983ec7e582b6e9978a39a99dbf1d43f1c500ae
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-06-15 20:06:54 +0530

    dev: Use .env file for Sentry DSN.
    * Reads .env file in root folder of repo to get Sentry DSN for builds.

--- app/renderer/js/utils/sentry-util.ts
+++ app/renderer/js/utils/sentry-util.ts
@@ -1,11 +1,14 @@
 import { init } from '@sentry/electron';
 
 import isDev = require('electron-is-dev');
+import path = require('path');
+import dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '/../../../../.env') });
 
 export const sentryInit = (): void => {
 	if (!isDev) {
 		init({
-			dsn: 'SENTRY_DSN',
+			dsn: process.env.SENTRY_DSN,
 			// We should ignore this error since it's harmless and we know the reason behind this
 			// This error mainly comes from the console logs.
 			// This is a temp solution until Sentry supports disabling the console logs
