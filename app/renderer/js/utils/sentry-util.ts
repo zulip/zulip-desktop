b7d28e20afe67bddb30bbcd06a087fb77a20576f
vsvipul <vsvipul555@gmail.com>
2019-06-16 21:43:53 +0530

    typescript: Migrate sentry-util.js to typescript.

---
+++ app/renderer/js/utils/sentry-util.ts
@@ -0,0 +1,17 @@
+import { init } from '@sentry/electron';
+import * as isDev from 'electron-is-dev';
+
+export const sentryInit = (): void => {
+	if (!isDev) {
+		init({
+			dsn: 'SENTRY_DSN',
+			// We should ignore this error since it's harmless and we know the reason behind this
+			// This error mainly comes from the console logs.
+			// This is a temp solution until Sentry supports disabling the console logs
+			ignoreErrors: ['does not appear to be a valid Zulip server']
+			// sendTimeout: 30 // wait 30 seconds before considering the sending capture to have failed, default is 1 second
+		});
+	}
+};
+
+export { captureException } from '@sentry/electron';
