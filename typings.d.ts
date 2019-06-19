d085664b70692392d0908b3a4d3e453c477482bf
Priyank Patel <priyankp390@gmail.com>
2019-06-19 13:59:31 -0400

    typing.d.ts: Add comment for why we use declare module.

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,5 @@
+// TODO: TypeScript - Remove this when the typescript migration
+// is done. Ideally we would do declare module only for our modules
+// that are not yet converted but relative path module declaration
+// are not supported
 declare module '*';
