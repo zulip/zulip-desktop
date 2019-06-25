73e0e81abd3c1c18d3a9127618323a491fdf1abd
vsvipul <vsvipul555@gmail.com>
2019-06-25 19:09:13 +0530

    gulp: Add tasks for compile and run from src.

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,37 @@ const electron = require('electron-connect').server.create({
 });
 const tape = require('gulp-tape');
 const tapColorize = require('tap-colorize');
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+
+const glob = require('glob');
+const { execSync } = require('child_process');
+
+const baseFilePattern = 'app/+(main|renderer)/**/*';
+const globOptions = { cwd: __dirname };
+const jsFiles = glob.sync(baseFilePattern + '.js', globOptions);
+const tsFiles = glob.sync(baseFilePattern + '.ts', globOptions);
+if (jsFiles.length !== tsFiles.length) {
+	const npx = process.platform === 'win32' ? 'npx.cmd' : 'npx';
+	console.log('Compiling typescript files...');
+	execSync(`${npx} tsc`);
+}
+
+gulp.task("compile", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("app"));
+});
 
 gulp.task('dev', () => {
   // Start browser process
 	electron.start();
   // Restart browser process
-	gulp.watch('app/main/*.js', gulp.series('restart:browser'));
+	gulp.watch('app/main/*.ts', gulp.series('compile', 'restart:browser'));
   // Reload renderer process
 	gulp.watch('app/renderer/css/*.css', gulp.series('reload:renderer'));
 	gulp.watch('app/renderer/*.html', gulp.series('reload:renderer'));
-	gulp.watch('app/renderer/js/**/*.js', gulp.series('reload:renderer'));
+	gulp.watch('app/renderer/js/**/*.ts', gulp.series('compile', 'reload:renderer'));
 });
 
 gulp.task('restart:browser', done => {
