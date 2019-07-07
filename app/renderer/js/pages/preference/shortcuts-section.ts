72fb61c49e4491601812572be7ef55c57a3a756e
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-07-07 16:02:24 +0530

    i18n: Add translations for shortcuts pane.

--- app/renderer/js/pages/preference/shortcuts-section.ts
+++ app/renderer/js/pages/preference/shortcuts-section.ts
@@ -3,6 +3,7 @@
 import { shell } from 'electron';
 
 import BaseSection = require('./base-section');
+import t = require('../../utils/translation-util');
 
 class ShortcutsSection extends BaseSection {
 	// TODO: TypeScript - Here props should be object type
@@ -20,143 +21,143 @@ class ShortcutsSection extends BaseSection {
 
 		return `
 						<div class="settings-pane">
-						<div class="settings-card tip"><p><b><i class="material-icons md-14">settings</i>Tip:  </b>These desktop app shortcuts extend the Zulip webapp's <span id="open-hotkeys-link"> keyboard shortcuts</span>.</p></div>
-							<div class="title">Application Shortcuts</div>
+						<div class="settings-card tip"><p><b><i class="material-icons md-14">settings</i>${t.__('Tip')}:  </b>${t.__('These desktop app shortcuts extend the Zulip webapp\'s')} <span id="open-hotkeys-link"> ${t.__('keyboard shortcuts')}</span>.</p></div>
+							<div class="title">${t.__('Application Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>,</kbd></td>
-										<td>Settings</td>
+										<td>${t.__('Settings')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>K</kbd></td>
-										<td>Keyboard Shortcuts</td>
+										<td>${t.__('Keyboard Shortcuts')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>M</kbd></td>
-										<td>Toggle Do Not Disturb</td>
+										<td>${t.__('Toggle Do Not Disturb')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>D</kbd></td>
-										<td>Reset App Settings</td>
+										<td>${t.__('Reset App Settings')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>L</kbd></td>
-										<td>Log Out</td>
+										<td>${t.__('Log Out')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>H</kbd></td>
-										<td>Hide Zulip</td>
+										<td>${t.__('Hide Zulip')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Option</kbd><kbd>${userOSKey}</kbd><kbd>H</kbd></td>
-										<td>Hide Others</td>
+										<td>${t.__('Hide Others')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>Q</kbd></td>
-										<td>Quit Zulip</td>
+										<td>${t.__('Quit Zulip')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">Edit Shortcuts</div>
+							<div class="title">${t.__('Edit Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>Z</kbd></td>
-										<td>Undo</td>
+										<td>${t.__('Undo')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>Z</kbd></td>
-										<td>Redo</td>
+										<td>${t.__('Redo')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>X</kbd></td>
-										<td>Cut</td>
+										<td>${t.__('Cut')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>C</kbd></td>
-										<td>Copy</td>
+										<td>${t.__('Copy')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>V</kbd></td>
-										<td>Paste</td>
+										<td>${t.__('Paste')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>V</kbd></td>
-										<td>Paste and Match Style</td>
+										<td>${t.__('Paste and Match Style')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>A</kbd></td>
-										<td>Select All</td>
+										<td>${t.__('Select All')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Control</kbd><kbd>${userOSKey}</kbd><kbd>Space</kbd></td>
-										<td>Emoji & Symbols</td>
+										<td>${t.__('Emoji & Symbols')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">View Shortcuts</div>
+							<div class="title">${t.__('View Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>R</kbd></td>
-										<td>Reload</td>
+										<td>${t.__('Reload')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Shift</kbd><kbd>${userOSKey}</kbd><kbd>R</kbd></td>
-										<td>Hard Reload</td>
+										<td>${t.__('Hard Reload')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Control</kbd><kbd>${userOSKey}</kbd><kbd>F</kbd></td>
-										<td>Enter Full Screen</td>
+										<td>${t.__('Enter Full Screen')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>+</kbd></td>
-										<td>Zoom In</td>
+										<td>${t.__('Zoom In')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>-</kbd></td>
-										<td>Zoom Out</td>
+										<td>${t.__('Zoom Out')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>0</kbd></td>
-										<td>Actual Size</td>
+										<td>${t.__('Actual Size')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>S</kbd></td>
-										<td>Toggle Sidebar</td>
+										<td>${t.__('Toggle Sidebar')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Option</kbd><kbd>${userOSKey}</kbd><kbd>I</kbd></td>
-										<td>Toggle DevTools for Zulip App</td>
+										<td>${t.__('Toggle DevTools for Zulip App')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Option</kbd><kbd>${userOSKey}</kbd><kbd>U</kbd></td>
-										<td>Toggle DevTools for Active Tab</td>
+										<td>${t.__('Toggle DevTools for Active Tab')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Ctrl</kbd> + <kbd>Tab</kbd></td>
-										<td>Switch to Next Organization</td>
+										<td>${t.__('Switch to Next Organization')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>Tab</kbd></td>
-										<td>Switch to Previous Organization</td>
+										<td>${t.__('Switch to Previous Organization')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">History Shortcuts</div>
+							<div class="title">${t.__('History Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>←</kbd></td>
-										<td>Back</td>
+										<td>${t.__('Back')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>→</kbd></td>
-										<td>Forward</td>
+										<td>${t.__('Forward')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
@@ -166,11 +167,11 @@ class ShortcutsSection extends BaseSection {
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>M</kbd></td>
-										<td>Minimize</td>
+										<td>${t.__('Minimize')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd><kbd>W</kbd></td>
-										<td>Close</td>
+										<td>${t.__('Close')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
@@ -184,141 +185,141 @@ class ShortcutsSection extends BaseSection {
 
 		return `
 						<div class="settings-pane">
-						<div class="settings-card tip"><p><b><i class="material-icons md-14">settings</i>Tip:  </b>These desktop app shortcuts extend the Zulip webapp's <span id="open-hotkeys-link"> keyboard shortcuts</span>.</p></div>
-							<div class="title">Application Shortcuts</div>
+						<div class="settings-card tip"><p><b><i class="material-icons md-14">settings</i>${t.__('Tip')}:  </b>${t.__('These desktop app shortcuts extend the Zulip webapp\'s')} <span id="open-hotkeys-link"> ${t.__('keyboard shortcuts')}</span>.</p></div>
+							<div class="title">${t.__('Application Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>,</kbd></td>
-										<td>Settings</td>
+										<td>${t.__('Settings')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>K</kbd></td>
-										<td>Keyboard Shortcuts</td>
+										<td>${t.__('Keyboard Shortcuts')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>M</kbd></td>
-										<td>Toggle Do Not Disturb</td>
+										<td>${t.__('Toggle Do Not Disturb')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>L</kbd></td>
-										<td>Log Out</td>
+										<td>${t.__('Log Out')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Q</kbd></td>
-										<td>Quit Zulip</td>
+										<td>${t.__('Quit Zulip')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">Edit Shortcuts</div>
+							<div class="title">${t.__('Edit Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Z</kbd></td>
-										<td>Undo</td>
+										<td>${t.__('Undo')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Y</kbd></td>
-										<td>Redo</td>
+										<td>${t.__('Redo')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>X</kbd></td>
-										<td>Cut</td>
+										<td>${t.__('Cut')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>C</kbd></td>
-										<td>Copy</td>
+										<td>${t.__('Copy')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>V</kbd></td>
-										<td>Paste</td>
+										<td>${t.__('Paste')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>V</kbd></td>
-										<td>Paste and Match Style</td>
+										<td>${t.__('Paste and Match Style')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>A</kbd></td>
-										<td>Select All</td>
+										<td>${t.__('Select All')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">View Shortcuts</div>
+							<div class="title">${t.__('View Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>R</kbd></td>
-										<td>Reload</td>
+										<td>${t.__('Reload')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>R</kbd></td>
-										<td>Hard Reload</td>
+										<td>${t.__('Hard Reload')}</td>
 									</tr>
 									<tr>
 										<td><kbd>F11</kbd></td>
-										<td>Toggle Full Screen</td>
+										<td>${t.__('Toggle Full Screen')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>=</kbd></td>
-										<td>Zoom In</td>
+										<td>${t.__('Zoom In')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>-</kbd></td>
-										<td>Zoom Out</td>
+										<td>${t.__('Zoom Out')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>0</kbd></td>
-										<td>Actual Size</td>
+										<td>${t.__('Actual Size')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>S</kbd></td>
-										<td>Toggle Sidebar</td>
+										<td>${t.__('Toggle Sidebar')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>I</kbd></td>
-										<td>Toggle DevTools for Zulip App</td>
+										<td>${t.__('Toggle DevTools for Zulip App')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>U</kbd></td>
-										<td>Toggle DevTools for Active Tab</td>
+										<td>${t.__('Toggle DevTools for Active Tab')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Tab</kbd></td>
-										<td>Switch to Next Organization</td>
+										<td>${t.__('Switch to Next Organization')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>Shift</kbd> + <kbd>Tab</kbd></td>
-										<td>Switch to Previous Organization</td>
+										<td>${t.__('Switch to Previous Organization')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">History Shortcuts</div>
+							<div class="title">${t.__('History Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>Alt</kbd> + <kbd>←</kbd></td>
-										<td>Back</td>
+										<td>${t.__('Back')}</td>
 									</tr>
 									<tr>
 										<td><kbd>Alt</kbd> + <kbd>→</kbd></td>
-										<td>Forward</td>
+										<td>${t.__('Forward')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
 							</div>
-							<div class="title">Window Shortcuts</div>
+							<div class="title">${t.__('Window Shortcuts')}</div>
 							<div class="settings-card">
 								<table>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>M</kbd></td>
-										<td>Minimize</td>
+										<td>${t.__('Minimize')}</td>
 									</tr>
 									<tr>
 										<td><kbd>${userOSKey}</kbd> + <kbd>W</kbd></td>
-										<td>Close</td>
+										<td>${t.__('Close')}</td>
 									</tr>
 								</table>
 								<div class="setting-control"></div>
