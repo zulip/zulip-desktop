c3f5405e815013424a75b3503a94d01210af4d1b
vsvipul <vsvipul555@gmail.com>
2019-07-26 19:38:43 +0530

    BrowserView: Improve ipc names.

--- app/renderer/js/main.ts
+++ app/renderer/js/main.ts
@@ -345,7 +345,7 @@ class ServerManagerView {
 			nodeIntegration: false,
 			preload: true
 		};
-		ipcRenderer.send('view-create', props);
+		ipcRenderer.send('create-view', props);
 		this.loading.add(server.url);
 	}
 
@@ -459,7 +459,7 @@ class ServerManagerView {
 			nodeIntegration: true,
 			preload: false
 		};
-		ipcRenderer.send('view-create', props);
+		ipcRenderer.send('create-view', props);
 		// To show loading indicator the first time a functional tab is opened, indicator is
 		// closed when the functional tab DOM is ready, handled in webview.js
 		this.$webviewsContainer.classList.remove('loaded');
@@ -474,7 +474,7 @@ class ServerManagerView {
 			url: `file://${rendererDirectory}/preference.html#${nav}`
 		});
 		this.$settingsButton.classList.add('active');
-		ipcRenderer.send('forward-message-view', 'switch-settings-nav', nav);
+		ipcRenderer.send('forward-view-message', 'switch-settings-nav', nav);
 	}
 
 	openAbout(): void {
@@ -533,7 +533,7 @@ class ServerManagerView {
 
 		this.activeTabIndex = index;
 		this.tabs[index].activate();
-		ipcRenderer.send('view-select', index);
+		ipcRenderer.send('select-view', index);
 		this.showLoading(this.loading.has(this.tabs[this.activeTabIndex].props.url));
 		ipcRenderer.send('call-view-function', 'canGoBackButton');
 		ipcRenderer.send('update-menu', {
@@ -572,7 +572,7 @@ class ServerManagerView {
 		// Show loading indicator
 		this.$webviewsContainer.classList.remove('loaded');
 
-		ipcRenderer.send('view-destroy-all');
+		ipcRenderer.send('destroy-all-views');
 
 		// Clear global variables
 		this.activeTabIndex = -1;
@@ -614,7 +614,7 @@ class ServerManagerView {
 	}
 
 	updateGeneralSettings(setting: string, value: unknown): void {
-		ipcRenderer.send('forward-message-view', setting, value);
+		ipcRenderer.send('forward-view-message', setting, value);
 	}
 
 	toggleSidebar(show: boolean): void {
@@ -819,7 +819,7 @@ class ServerManagerView {
 		ipcRenderer.on('toggle-dnd', (event: Event, state: boolean, newSettings: DNDSettings) => {
 			this.toggleDNDButton(state);
 			ipcRenderer.send('forward-message', 'toggle-silent', newSettings.silent);
-			ipcRenderer.send('forward-message-view', 'toggle-dnd', state, newSettings);
+			ipcRenderer.send('forward-view-message', 'toggle-dnd', state, newSettings);
 		});
 
 		ipcRenderer.on('update-realm-name', (event: Event, serverURL: string, realmName: string) => {
