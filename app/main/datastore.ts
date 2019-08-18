5e95c77568e3bdfa8d8200c2ebbec0a38c4300c5
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-08-18 21:59:06 +0530

    leveldb: Add convertDB() to migrate from old db.

--- app/main/datastore.ts
+++ app/main/datastore.ts
@@ -1,5 +1,7 @@
-import { BrowserWindow } from 'electron';
+import { app, dialog, BrowserWindow } from 'electron';
 
+import path = require('path');
+import fs = require('fs');
 import LevelDB = require('./leveldb');
 import Logger = require('../renderer/js/utils/logger-util');
 
@@ -24,7 +26,53 @@ class DataStore {
 		this.loadDomains();
 	}
 
+	async convertDB(databasePath: string): Promise<void> {
+		let legacyDB = null;
+		try {
+			const file = fs.readFileSync(databasePath, 'utf8');
+			// necessary to catch errors in JSON
+			legacyDB = JSON.parse(file);
+		} catch (err) {
+			if (fs.existsSync(databasePath)) {
+				fs.unlinkSync(databasePath);
+				dialog.showErrorBox(
+					'Error saving new organization',
+					'There seems to be error while saving new organization, ' +
+					'you may have to re-add your previous organizations back.'
+				);
+				logger.error('Error while JSON parsing domain.json: ');
+				logger.error(err);
+				logger.reportSentry(err);
+			}
+			return;
+		}
+		if (legacyDB) {
+			if (databasePath.includes('domain')) {
+				await this.domainsDB.put('domains', legacyDB.domains);
+				this.domains = legacyDB.domains;
+			} else {
+				let batch = this.settingsDB.batch();
+				for (const key in legacyDB) {
+					if (legacyDB.hasOwnProperty(key)) {
+						batch = batch.put(key, legacyDB[key]);
+					}
+				}
+				await batch.write();
+				this.settings = legacyDB;
+			}
+		}
+		if (fs.existsSync(databasePath)) {
+			// delete legacy database to complete migration.
+			fs.unlinkSync(databasePath);
+		}
+	}
+
 	loadSettings(): void {
+		const databasePath = path.join(app.getPath('userData'), 'config/settings.json');
+		if (fs.existsSync(databasePath)) {
+			this.convertDB(databasePath);
+			return;
+		}
 		this.settingsDB.createReadStream().on('data', (configItem: any) => {
 			if (configItem.value === '__null__') {
 				configItem.value = null;
@@ -37,6 +85,11 @@ class DataStore {
 	}
 
 	loadDomains(): void {
+		const databasePath = path.join(app.getPath('userData'), 'config/domain.json');
+		if (fs.existsSync(databasePath)) {
+			this.convertDB(databasePath);
+			return;
+		}
 		this.domainsDB.createReadStream().on('data', (domains: any) => {
 			this.domains = domains.value;
 			this.updateUtil('domain-update');
