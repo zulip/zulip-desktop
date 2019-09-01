e67a259eb144e0321f74450ee1966a1f5f45dfdf
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-09-01 23:21:18 +0530

    network: Edit network error message, add settings.

--- app/renderer/js/pages/network.ts
+++ app/renderer/js/pages/network.ts
@@ -4,13 +4,18 @@ import { ipcRenderer } from 'electron';
 
 class NetworkTroubleshootingView {
 	$reconnectButton: Element;
+	$settingsButton: Element;
 	constructor() {
 		this.$reconnectButton = document.querySelector('#reconnect');
+		this.$settingsButton = document.querySelector('#settings');
 	}
 
 	init(): void {
 		this.$reconnectButton.addEventListener('click', () => {
 			ipcRenderer.send('forward-message', 'reload-viewer');
+		});
+		this.$settingsButton.addEventListener('click', () => {
+			ipcRenderer.send('forward-message', 'open-settings');
 		});
 	}
 }
