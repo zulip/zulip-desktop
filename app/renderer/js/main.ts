8446deb673c88c2b433f9f34ee09f3c1f048d0cf
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-09-25 18:25:00 +0530

    sidebar: Improve UX for notification settings.
    * sidebar: Disable notif settings if not logged in.
    
    * sidebar: Activate relevant tab for notif settings.

--- app/renderer/js/main.ts
+++ app/renderer/js/main.ts
@@ -731,6 +731,11 @@ class ServerManagerView {
 		this.$dndButton.querySelector('i').textContent = alert ? 'notifications_off' : 'notifications';
 	}
 
+	isLoggedIn(tabIndex: number): boolean {
+		const url = this.tabs[tabIndex].webview.$el.src;
+		return !(url.endsWith('/login/') || this.tabs[tabIndex].webview.loading);
+	}
+
 	addContextMenu($serverImg: HTMLImageElement, index: number): void {
 		$serverImg.addEventListener('contextmenu', e => {
 			e.preventDefault();
@@ -757,8 +762,11 @@ class ServerManagerView {
 				},
 				{
 					label: 'Notification settings',
+					enabled: this.isLoggedIn(index),
 					click: () => {
-						this.tabs[this.activeTabIndex].webview.showNotificationSettings();
+						// switch to tab whose icon was right-clicked
+						this.activateTab(index);
+						this.tabs[index].webview.showNotificationSettings();
 					}
 				},
 				{
