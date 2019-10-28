0a50f268f5c640a9c5de13adf5912b5fc76d8b16
Swapnilr1 <swapnilrustagi97792@gmail.com>
2019-10-28 13:59:05 +0530

    Remove extra code from module (debugging)

--- app/renderer/js/notification/winrt-notifications.ts
+++ app/renderer/js/notification/winrt-notifications.ts
@@ -1,46 +1,15 @@
 'use strict';
 // import { ipcRenderer } from 'electron';
-// import { appId } from './helpers';
-import {
-	appId, customReply, focusCurrentServer, parseReply, setupReply
-} from './helpers';
+import { appId } from './helpers';
+// import {
+// 	appId, customReply, focusCurrentServer, parseReply, setupReply
+// } from './helpers';
 
 // import url = require('url');
 import WinRTNotifier = require('electron-windows-notifications');
 
 import ConfigUtil = require('../utils/config-util');
 
-type ReplyHandler = (response: string) => void;
-type ClickHandler = () => void;
-let replyHandler: ReplyHandler;
-let clickHandler: ClickHandler;
-console.log('ABC');
-declare const window: ZulipWebWindow;
-interface NotificationHandlerArgs {
-	response: string;
-}
-
-const profilePic2 = "F:\\GitHub\\zulip-desktop\\app\\renderer\\img\\icon.png";
-// this.tag = opts.tag;
-const notification2 = new WinRTNotifier.ToastNotification({
-	appId,
-	template: `<toast launch="message">
-			<visual>
-			<binding template="ToastGeneric">
-				   <text hint-maxLines="1">%s</text>
-				   <image placement="appLogoOverride" hint-crop="circle" src="%s"/>
-			<text>%s</text>
-		</binding>
-		</visual>
-		<actions>
-			<input id="reply" type="text" placeHolderContent="Type a reply"/>	
-			<action hint-inputId="" activationType="background" content="Reply" arguments="send" />
-			<action activationType="background" content="Dismiss" arguments="" />
-		</actions>			
-		</toast>`,
-	strings: ['Sender', profilePic2, 'Message']
-});
-notification2.show();
 
 class WinRTNotification {
 	tag: string;
@@ -51,10 +20,13 @@ class WinRTNotification {
 		// const { icon } = opts;
 		// const profilePic = url.resolve(`${protocol}//${host}`, icon);
 		const profilePic = "F:\\GitHub\\zulip-desktop\\app\\renderer\\img\\icon.png";
+		// const notifTitle = title;
 		this.tag = opts.tag;
 		opts.silent = false;
 		console.log('Executed');
-		const notification = new WinRTNotifier.ToastNotification({
+		console.log(title);
+		console.log(opts);
+		const notification2 = new WinRTNotifier.ToastNotification({
 			appId,
 			template: `<toast launch="message">
 					<visual>
@@ -70,9 +42,12 @@ class WinRTNotification {
 					<action activationType="background" content="Dismiss" arguments="" />
 				</actions>			
 				</toast>`,
-			strings: [title, profilePic, 'Message']
+			strings: ['Sender', profilePic, 'Message']
 		});
-		notification.show();
+		console.log("X123");
+		notification2.show();
+		console.log("Created notification");
+		// notification.show();
 	}
 
 	static requestPermission(): void {
@@ -82,52 +57,6 @@ class WinRTNotification {
 	// Override default Notification permission
 	static get permission(): NotificationPermission {
 		return ConfigUtil.getConfigItem('showNotification') ? 'granted' : 'denied';
-	}
-
-	set onreply(handler: ReplyHandler) {
-		replyHandler = handler;
-	}
-
-	get onreply(): ReplyHandler {
-		return replyHandler;
-	}
-
-	set onclick(handler: ClickHandler) {
-		clickHandler = handler;
-	}
-
-	get onclick(): ClickHandler {
-		return clickHandler;
-	}
-
-	// not something that is common or
-	// used by zulip server but added to be
-	// future proff.
-	addEventListener(event: string, handler: ClickHandler | ReplyHandler): void {
-		if (event === 'click') {
-			clickHandler = handler as ClickHandler;
-		}
-
-		if (event === 'reply') {
-			replyHandler = handler as ReplyHandler;
-		}
-	}
-
-	notificationHandler({ response }: NotificationHandlerArgs): void {
-		response = parseReply(response);
-		focusCurrentServer();
-		if (window.electron_bridge.send_notification_reply_message_supported) {
-			window.electron_bridge.send_event('send_notification_reply_message', this.tag, response);
-			return;
-		}
-
-		setupReply(this.tag);
-		if (replyHandler) {
-			replyHandler(response);
-			return;
-		}
-
-		customReply(response);
 	}
 }
 
