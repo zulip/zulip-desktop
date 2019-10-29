f0c7ebc52b8e9ee2b349302db33a64c7506be583
Swapnilr1 <swapnilrustagi97792@gmail.com>
2019-10-30 00:27:54 +0530

    Interactive notification displaying with a caveat

--- app/renderer/js/notification/winrt-notifications.ts
+++ app/renderer/js/notification/winrt-notifications.ts
@@ -7,47 +7,44 @@ import { appId } from './helpers';
 
 // import url = require('url');
 import WinRTNotifier = require('electron-windows-notifications');
-
+import DefaultNotification = require('./default-notification');
 import ConfigUtil = require('../utils/config-util');
 
+function showNotif(title: string, opts: NotificationOptions): void {
+	const profilePic = "F:\\GitHub\\zulip-desktop\\app\\renderer\\img\\icon.png";
+	const notification2 = new WinRTNotifier.ToastNotification({
+		appId,
+		template: `<toast launch="message">
+				<visual>
+				<binding template="ToastGeneric">
+					   <text hint-maxLines="1">%s</text>
+					   <image placement="appLogoOverride" hint-crop="circle" src="%s"/>
+				<text>%s</text>
+			</binding>
+			</visual>
+			<actions>
+				<input id="reply" type="text" placeHolderContent="Type a reply"/>	
+				<action hint-inputId="" activationType="background" content="Reply" arguments="send" />
+				<action activationType="background" content="Dismiss" arguments="" />
+			</actions>			
+			</toast>`,
+		strings: [title, profilePic, opts.body]
+		// tag: opts.tag
+	});
+	notification2.show();
+}
 
-class WinRTNotification {
-	tag: string;
-
+class WinRTNotification extends DefaultNotification {
+	// tag: string;
 	constructor(title: string, opts: NotificationOptions) {
-		// const silent: boolean = ConfigUtil.getConfigItem('silent') || false;
-		// const { host, protocol } = location;
-		// const { icon } = opts;
-		// const profilePic = url.resolve(`${protocol}//${host}`, icon);
-		const profilePic = "F:\\GitHub\\zulip-desktop\\app\\renderer\\img\\icon.png";
-		// const notifTitle = title;
-		this.tag = opts.tag;
+		opts.silent = true;
+		super(title, opts);
+		// this.tag = opts.tag;
 		opts.silent = false;
-		console.log('Executed');
 		console.log(title);
 		console.log(opts);
-		const notification2 = new WinRTNotifier.ToastNotification({
-			appId,
-			template: `<toast launch="message">
-					<visual>
-					<binding template="ToastGeneric">
-						   <text hint-maxLines="1">%s</text>
-						   <image placement="appLogoOverride" hint-crop="circle" src="%s"/>
-					<text>%s</text>
-				</binding>
-				</visual>
-				<actions>
-					<input id="reply" type="text" placeHolderContent="Type a reply"/>	
-					<action hint-inputId="" activationType="background" content="Reply" arguments="send" />
-					<action activationType="background" content="Dismiss" arguments="" />
-				</actions>			
-				</toast>`,
-			strings: ['Sender', profilePic, 'Message']
-		});
-		console.log("X123");
-		notification2.show();
+		showNotif(title, opts);
 		console.log("Created notification");
-		// notification.show();
 	}
 
 	static requestPermission(): void {
@@ -58,6 +55,15 @@ class WinRTNotification {
 	static get permission(): NotificationPermission {
 		return ConfigUtil.getConfigItem('showNotification') ? 'granted' : 'denied';
 	}
+
+	close(): void {
+		return; // eslint-disable-line no-useless-return
+	}
+
+	// show(): void{
+	// 	this.notification2.show();
+	// 	return;
+	// }
 }
 
 module.exports = WinRTNotification;
