787035baa7df1cd1d8ca1ab0177b97bae9000ccf
Akash Nimare <akashnimare@users.noreply.github.com>
2019-11-12 17:46:13 +0530

    notarization: update env path.

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: path.resolve(__dirname, '/../.env') });
+dotenv.config({ path: path.join(__dirname, '/../.env') });
 
 const { notarize } = require('electron-notarize');
 
