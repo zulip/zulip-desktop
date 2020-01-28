1d9a923245b8f1e6e29b38629f698f93adeaa27e
ViPuL <vsvipul555@gmail.com>
2020-01-28 12:26:41 +0530

    startup: Use inbuilt electron API for autostartup. (#859)
    Switch from using the external auto-launch module to
    inbuilt setLoginItemSettings for windows and macOS,
    as some users reported issues on windows.
    
    Fixes: #851.

--- app/main/startup.ts
+++ app/main/startup.ts
@@ -11,21 +11,23 @@ export const setAutoLaunch = (AutoLaunchValue: boolean): void => {
 		return;
 	}
 
-	// On Mac, work around a bug in auto-launch where it opens a Terminal window
-	// See https://github.com/Teamwork/node-auto-launch/issues/28#issuecomment-222194437
-
-	const appPath = process.platform === 'darwin' ? app.getPath('exe').replace(/\.app\/Content.*/, '.app') : undefined; // Use the default
-
-	const ZulipAutoLauncher = new AutoLaunch({
-		name: 'Zulip',
-		path: appPath,
-		isHidden: false
-	});
 	const autoLaunchOption = ConfigUtil.getConfigItem('startAtLogin', AutoLaunchValue);
 
-	if (autoLaunchOption) {
-		ZulipAutoLauncher.enable();
+	// setLoginItemSettings doesn't support linux
+	if (process.platform === 'linux') {
+		const ZulipAutoLauncher = new AutoLaunch({
+			name: 'Zulip',
+			isHidden: false
+		});
+		if (autoLaunchOption) {
+			ZulipAutoLauncher.enable();
+		} else {
+			ZulipAutoLauncher.disable();
+		}
 	} else {
-		ZulipAutoLauncher.disable();
+		app.setLoginItemSettings({
+			openAtLogin: autoLaunchOption,
+			openAsHidden: false
+		});
 	}
 };
