6036a44fb29e1b96deff1bb1027116e967e1dacc
Anders Kaseorg <anders@zulipchat.com>
2020-03-02 18:57:27 -0800

    new-server-form: Remove useless .server-save-action wrappers.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- tests/test-add-organization.js
+++ tests/test-add-organization.js
@@ -8,7 +8,7 @@ test('add-organization', function (t) {
   setup.waitForLoad(app, t)
     .then(() => app.client.windowByIndex(1)) // focus on webview
     .then(() => app.client.setValue('.setting-input-value', 'chat.zulip.org'))
-    .then(() => app.client.click('.server-save-action'))
+    .then(() => app.client.click('#connect'))
     .then(() => setup.wait(5000))
     .then(() => app.client.windowByIndex(0)) // Switch focus back to main win
     .then(() => app.client.windowByIndex(1)) // Switch focus back to org webview
