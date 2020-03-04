ca6b2312be0dd82a5d7a3a67a4c0b28d7d17c4fa
Anders Kaseorg <anders@zulipchat.com>
2020-03-03 18:16:01 -0800

    typings: Get Window.Notification from upstream.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- typings.d.ts
+++ typings.d.ts
@@ -9,24 +9,10 @@ interface PageParamsObject {
 }
 declare var page_params: PageParamsObject;
 
-// Patch Notification object so we can implement our side
-// of Notification classes which we export into zulip side through
-// preload.js; if we don't do his extending Notification will throw error.
-// Relevant code is in app/renderer/js/notification/default-notification.ts
-// and the relevant function is requestPermission.
-declare var PatchedNotification: {
-  prototype: Notification;
-  new(title: string, options?: NotificationOptions): Notification;
-  readonly maxActions: number;
-  readonly permission: NotificationPermission;
-  requestPermission(): void;
-}
-
 // This is mostly zulip side of code we access from window
 interface Window {
   $: any;
   narrow: any
-  Notification: typeof PatchedNotification;
 }
 
 interface ZulipWebWindow extends Window {
