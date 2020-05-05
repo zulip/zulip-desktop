4c25c99abcf277a327e1bb0a74310b8c6f7e14a3
Akash Nimare <svnitakash@gmail.com>
2020-05-06 01:13:29 +0530

    notarization: Pass team short name info in code-signing.

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: path.join(__dirname, '/../.env')});
 
-const {notarize} = require('electron-notarize');
+const { notarize } = require('electron-notarize');
 
 exports.default = async function (context) {
 	const {electronPlatformName, appOutDir} = context;
@@ -18,6 +18,7 @@ exports.default = async function (context) {
 		appBundleId: 'org.zulip.zulip-electron',
 		appPath: `${appOutDir}/${appName}.app`,
 		appleId: process.env.APPLE_ID,
-		appleIdPassword: process.env.APPLE_ID_PASS
+		appleIdPassword: process.env.APPLE_ID_PASS,
+		ascProvider: process.env.ASC_PROVIDER // team short name
 	});
 };
