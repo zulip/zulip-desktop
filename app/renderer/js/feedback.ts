bf651dece041d8971ab15044e9c4099abe767ec8
Priyank Patel <priyankp390@gmail.com>
2020-05-19 09:32:53 -0400

    report-issue: Remove unnessacary instructions from the placeholder.
    It looks like the removed instructions were directly copied from GitHub
    issue template and did not fit here. Specifically, the first point about
    including the platform is automatically included in the email we prepare for
    the user and we also include browser and electron version. Then the second
    bullet point re-iterates the instructions present. Lastly, the third point
    asks for screenshot however user cannot add screenshot in the UI but they can
    in the email.
    
    We also remove an comment disabling a eslint rule. The rule was disable for no
    reason because we should have used template literal instead of the undocumented
    multi-str format which is not cleaner.

--- app/renderer/js/feedback.ts
+++ app/renderer/js/feedback.ts
@@ -13,25 +13,12 @@ export const feedbackHolder = sendFeedback.parentElement;
 // Make the button color match zulip app's theme
 sendFeedback.customStylesheet = 'css/feedback.css';
 
-/* eslint-disable no-multi-str */
-
-// customize the fields of custom elements
+// Customize the fields of custom elements
 sendFeedback.title = 'Report Issue';
 sendFeedback.titleLabel = 'Issue title:';
 sendFeedback.titlePlaceholder = 'Enter issue title';
 sendFeedback.textareaLabel = 'Describe the issue:';
-sendFeedback.textareaPlaceholder = 'Succinctly describe your issue and steps to reproduce it...\n\n\
----\n\
-<!-- Please Include: -->\n\
-- **Operating System**:\n\
-  - [ ] Windows\n\
-  - [ ] Linux/Ubuntu\n\
-  - [ ] macOS\n\
-- **Clear steps to reproduce the issue**:\n\
-- **Relevant error messages and/or screenshots**:\n\
-';
-
-/* eslint-enable no-multi-str */
+sendFeedback.textareaPlaceholder = 'Succinctly describe your issue and steps to reproduce it...';
 
 sendFeedback.buttonLabel = 'Report Issue';
 sendFeedback.loaderSuccessText = '';
