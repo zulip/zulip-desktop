da8509559966d17628586e7f1bf2abd9c0084198
Manav Mehta <tmanavmehta@gmail.com>
2020-08-14 19:54:09 +0530

    darwin-notifications: Migrate from node-mac-notifier to node-notifier
    node-mac-notifier was less maintained as compared to node-notifier and some bugs were being noticed where notifications were not seen.
    This is a potential solution to that bug.

--- app/renderer/js/notification/darwin-notifications.ts
+++ app/renderer/js/notification/darwin-notifications.ts
@@ -1,40 +1,47 @@
 import {ipcRenderer} from 'electron';
 
-import MacNotifier from 'node-mac-notifier';
+import notifier from 'node-notifier';
+import NotificationCenter from 'node-notifier/notifiers/notificationcenter';
 
 import electron_bridge from '../electron-bridge';
 import * as ConfigUtil from '../utils/config-util';
 
-import {
-	appId, customReply, focusCurrentServer, parseReply
-} from './helpers';
+import {customReply, focusCurrentServer, parseReply} from './helpers';
 
 type ReplyHandler = (response: string) => void;
 type ClickHandler = () => void;
 let replyHandler: ReplyHandler;
 let clickHandler: ClickHandler;
 
-interface NotificationHandlerArgs {
-	response: string;
-}
-
 class DarwinNotification {
 	tag: number;
 
 	constructor(title: string, options: NotificationOptions) {
-		const silent: boolean = ConfigUtil.getConfigItem('silent') || false;
-		const {icon} = options;
-		const profilePic = new URL(icon, location.href).href;
-
+		// Const silent: boolean = ConfigUtil.getConfigItem('silent') || false;
+		const profilePic = new URL(options.icon, location.href).href;
 		this.tag = Number.parseInt(options.tag, 10);
-		const notification = new MacNotifier(title, Object.assign(options, {
-			bundleId: appId,
-			canReply: true,
-			silent,
-			icon: profilePic
-		}));
 
-		notification.addEventListener('click', () => {
+		const notification: NotificationCenter.Notification = {
+			title,
+			message: options.body,
+			icon: '../../../resources/Icon.icns',
+			contentImage: profilePic,
+			wait: true,
+			timeout: 1000, // If not large, response is timed out and reply is not sent
+			closeLabel: 'Close',
+			dropdownLabel: undefined,
+			reply: true
+		};
+
+		notifier.notify(notification, async (_error, _response, metadata) => {
+			console.log(_response);
+			console.log(metadata);
+			if (metadata.activationType === 'replied') {
+				await this.notificationHandler(metadata.activationValue);
+			}
+		});
+
+		notifier.on('click', () => {
 			// Focus to the server who sent the
 			// notification if not focused already
 			if (clickHandler) {
@@ -44,8 +51,6 @@ class DarwinNotification {
 			focusCurrentServer();
 			ipcRenderer.send('focus-app');
 		});
-
-		notification.addEventListener('reply', this.notificationHandler);
 	}
 
 	static requestPermission(): void {
@@ -86,7 +91,7 @@ class DarwinNotification {
 		}
 	}
 
-	async notificationHandler({response}: NotificationHandlerArgs): Promise<void> {
+	async notificationHandler(response: string): Promise<void> {
 		response = await parseReply(response);
 		focusCurrentServer();
 		if (electron_bridge.send_notification_reply_message_supported) {
