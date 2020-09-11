zulip/zulip-desktop

commit cf9d0c8aa2aa85386d9504621538cf68c55afb1c

main: Remove mainWindowState from globals.
(No, globals are not accessible from the other process.)

Signed-off-by: Anders Kaseorg <anders@zulip.com>

app/main/index.ts
@@ -13,11 +13,7 @@ import * as CertificateUtil from '../renderer/js/utils/certificate-util';
 import * as ConfigUtil from '../renderer/js/utils/config-util';
 import * as ProxyUtil from '../renderer/js/utils/proxy-util';
 
-interface PatchedGlobal extends NodeJS.Global {
-	mainWindowState: windowStateKeeper.State;
-}
-
-const globalPatched = global as PatchedGlobal;
+let mainWindowState: windowStateKeeper.State;
 
 // Prevent window being garbage collected
 let mainWindow: Electron.BrowserWindow;
@@ -76,9 +72,6 @@ function createMainWindow(): Electron.BrowserWindow {
 		defaultHeight: 720,
 		path: `${app.getPath('userData')}/config`
 	});
-
-	// Let's keep the window position global so that we can access it in other process
-	globalPatched.mainWindowState = mainWindowState;
 
 	const win = new electron.BrowserWindow({
 		// This settings needs to be saved in config
@@ -315,7 +308,7 @@ ${error}`
 	});
 
 	ipcMain.on('clear-app-settings', () => {
-		globalPatched.mainWindowState.unmanage();
+		mainWindowState.unmanage();
 		app.relaunch();
 		app.exit();
 	});

commit 3a3714787f00e7bce2fbf3e4303c4e864470c13b

main: Fix mainWindowState scope.
Fixes a regression with the factory reset function introduced by
commit cf9d0c8aa2aa85386d9504621538cf68c55afb1c.

Signed-off-by: Anders Kaseorg <anders@zulip.com>

app/main/index.ts
@@ -70,7 +70,7 @@ const toggleApp = (): void => {
 
 function createMainWindow(): Electron.BrowserWindow {
 	// Load the previous state with fallback to defaults
-	const mainWindowState: windowStateKeeper.State = windowStateKeeper({
+	mainWindowState = windowStateKeeper({
 		defaultWidth: 1100,
 		defaultHeight: 720,
 		path: `${app.getPath('userData')}/config`