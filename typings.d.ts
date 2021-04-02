815d9d4e2831fed0d92bf21da0ae91369c5dd5ea
Anders Kaseorg <anders@zulip.com>
2021-04-02 16:34:43 -0700

    typescript: Remove skipLibCheck.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- typings.d.ts
+++ typings.d.ts
@@ -1,4 +1,4 @@
-declare module "electron" {
+declare namespace Electron {
   // https://github.com/electron/typescript-definitions/issues/170
   interface IncomingMessage extends NodeJS.ReadableStream {}
 }
@@ -29,12 +29,12 @@ interface ClipboardDecrypter {
 
 interface ElectronBridge {
   send_event: (eventName: string | symbol, ...args: unknown[]) => boolean;
-  on_event: (eventName: string, listener: ListenerType) => void;
+  on_event: (eventName: string, listener: (...args: any[]) => void) => void;
   new_notification: (
     title: string,
     options: NotificationOptions,
     dispatch: (type: string, eventInit: EventInit) => boolean,
-  ) => NotificationData;
+  ) => import("./app/renderer/js/notification").NotificationData;
   get_idle_on_system: () => boolean;
   get_last_active_on_system: () => number;
   get_send_notification_reply_message_supported: () => boolean;
