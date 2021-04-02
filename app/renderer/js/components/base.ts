e477aed8ff184ede21268a9a66e20c9e967b0f59
Anders Kaseorg <anders@zulip.com>
2021-04-02 14:14:07 -0700

    generateNodeFromHTML: Check for null.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- app/renderer/js/components/base.ts
+++ app/renderer/js/components/base.ts
@@ -1,7 +1,12 @@
 import type {HTML} from "../../../common/html";
 
-export function generateNodeFromHTML(html: HTML): Element | null {
+export function generateNodeFromHTML(html: HTML): Element {
   const wrapper = document.createElement("div");
   wrapper.innerHTML = html.html;
+
+  if (wrapper.firstElementChild === null) {
+    throw new Error("No element found in HTML");
+  }
+
   return wrapper.firstElementChild;
 }
