753b244630923fa62db69d243fc34278df8fcdbd
Anders Kaseorg <anders@zulip.com>
2021-04-23 22:26:21 -0700

    tests: Convert if chain to switch statement.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- tests/setup.js
+++ tests/setup.js
@@ -84,16 +84,20 @@ async function endTest(app, t, error) {
 function getAppDataDir() {
   let base;
 
-  if (process.platform === "darwin") {
-    base = path.join(process.env.HOME, "Library", "Application Support");
-  } else if (process.platform === "linux") {
-    base = process.env.XDG_CONFIG_HOME
-      ? process.env.XDG_CONFIG_HOME
-      : path.join(process.env.HOME, ".config");
-  } else if (process.platform === "win32") {
-    base = process.env.APPDATA;
-  } else {
-    throw new Error("Could not detect app data dir base.");
+  switch (process.platform) {
+    case "darwin":
+      base = path.join(process.env.HOME, "Library", "Application Support");
+      break;
+    case "linux":
+      base = process.env.XDG_CONFIG_HOME
+        ? process.env.XDG_CONFIG_HOME
+        : path.join(process.env.HOME, ".config");
+      break;
+    case "win32":
+      base = process.env.APPDATA;
+      break;
+    default:
+      throw new Error("Could not detect app data dir base.");
   }
 
   console.log("Detected App Data Dir base:", base);
