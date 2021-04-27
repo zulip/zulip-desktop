b8c39858fb79da9f5c0e181284266e8b4e11dd1e
tarun8718 <tarunkumar8718@gmail.com>
2021-04-27 20:54:33 +0530

    tests: create a spectron_lib folder with common and config files.
    spectron folder only consists tests now.
    
    Signed-off-by: tarun8718 <tarunkumar8718@gmail.com>

--- tests/spectron/test-back-button.js
+++ tests/spectron/test-back-button.js
@@ -1,9 +1,9 @@
 "use strict";
 const test = require("tape");
 
-const setup = require("./setup");
+const setup = require("../spectron_lib/common");
 
-test("add-organization", async (t) => {
+test("back button", async (t) => {
   t.timeoutAfter(50e3);
   setup.resetTestDataDir();
   const app = setup.createApp();
@@ -17,7 +17,18 @@ test("add-organization", async (t) => {
     await setup.wait(5000);
     await app.client.windowByIndex(0); // Switch focus back to main win
     await app.client.windowByIndex(1); // Switch focus back to org webview
-    await (await app.client.$('//*[@id="id_username"]')).waitForExist();
+    await (await app.client.$("#id_username")).waitForExist();
+    await (await app.client.$("#id_username")).setValue(
+      "testzulipdesktop@gmail.com",
+    );
+    await (await app.client.$("#id_password")).setValue("testzulipdesktop");
+    await (await app.client.$(".full-width")).click();
+    await setup.wait(5000);
+    await (await app.client.$(".private_messages_header")).click();
+    await setup.wait(5000);
+    await app.client.windowByIndex(0);
+    await (await app.client.$("#back-action")).click();
+    await setup.wait(5000);
     await setup.endTest(app, t);
   } catch (error) {
     await setup.endTest(app, t, error || "error");
--- tests/spectron/test-new-organization.js
+++ tests/spectron/test-new-organization.js
@@ -1,16 +1,19 @@
 "use strict";
 const test = require("tape");
 
-const setup = require("./setup");
+const setup = require("../spectron_lib/common");
 
-test("app runs", async (t) => {
-  t.timeoutAfter(10e3);
+// Create new org link should open in the default browser [WIP]
+
+test("new-org-link", async (t) => {
+  t.timeoutAfter(50e3);
   setup.resetTestDataDir();
   const app = setup.createApp();
   try {
     await setup.waitForLoad(app, t);
     await app.client.windowByIndex(1); // Focus on webview
-    await (await app.client.$('//*[@id="connect"]')).waitForExist(); // Id of the connect button
+    await (await app.client.$("#open-create-org-link")).click(); // Click on new org link button
+    await setup.wait(5000);
     await setup.endTest(app, t);
   } catch (error) {
     await setup.endTest(app, t, error || "error");
