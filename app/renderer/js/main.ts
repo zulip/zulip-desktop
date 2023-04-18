1da3ec545a3e70b93650c09ac39ada6ee3f657da
Anders Kaseorg <anders@zulip.com>
2023-04-18 13:09:24 -0700

    Don’t show visual notifications when they’re turned off.
    Fixes #1299.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- app/renderer/js/main.ts
+++ app/renderer/js/main.ts
@@ -392,7 +392,9 @@ export class ServerManagerView {
           url: server.url,
           role: "server",
           hasPermission: (origin: string, permission: string) =>
-            origin === server.url && permission === "notifications",
+            origin === server.url &&
+            permission === "notifications" &&
+            ConfigUtil.getConfigItem("showNotification", true),
           isActive: () => index === this.activeTabIndex,
           switchLoading: async (loading: boolean, url: string) => {
             if (loading) {
