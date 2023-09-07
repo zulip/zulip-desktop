b81660429aa780c16e12822b0a14441881d1eda8
RachitSrivastava12 <123284193+RachitSrivastava12@users.noreply.github.com>
2023-09-07 07:11:53 +0530

    Update notarize.js : As the Zulip desktop app is mostly written in typescript , converting this code to typescript

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,25 +1,39 @@
-"use strict";
-const path = require("node:path");
-const process = require("node:process");
+import * as path from "node:path";
+import * as process from "node:process";
+import { notarize, NotarizeOptions } from "@electron/notarize";
+import dotenv from "dotenv";
 
-const {notarize} = require("@electron/notarize");
-const dotenv = require("dotenv");
+dotenv.config({ path: path.join(__dirname, "/../.env") });
 
-dotenv.config({path: path.join(__dirname, "/../.env")});
-
-exports.default = async function (context) {
-  const {electronPlatformName, appOutDir} = context;
+export default async function notarizeApp(context: NotarizeContext): Promise<void> {
+  const { electronPlatformName, appOutDir, packager } = context;
   if (electronPlatformName !== "darwin") {
     return;
   }
 
-  const appName = context.packager.appInfo.productFilename;
+  const appName = packager.appInfo.productFilename;
 
-  return notarize({
+  const notarizeOptions: NotarizeOptions = {
     appBundleId: "org.zulip.zulip-electron",
     appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
-    ascProvider: process.env.ASC_PROVIDER, // Team short name
-  });
-};
+    appleId: process.env.APPLE_ID || "",
+    appleIdPassword: process.env.APPLE_ID_PASS || "",
+    ascProvider: process.env.ASC_PROVIDER || "", // Team short name
+  };
+
+  try {
+    await notarize(notarizeOptions);
+  } catch (error) {
+    console.error("Error notarizing the app:", error);
+  }
+}
+
+interface NotarizeContext {
+  electronPlatformName: string;
+  appOutDir: string;
+  packager: {
+    appInfo: {
+      productFilename: string;
+    };
+  };
+}
