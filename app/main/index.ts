6ebb9be25ba284167aa720ffd48a65a7babeb8eb
Avijit Das <rdas9237@gmail.com>
2025-02-22 17:39:24 +0530

    Removing some extra code #820
    Remove some extra code and which is added on previous commit

--- app/main/index.ts
+++ app/main/index.ts
@@ -1,4 +1,4 @@
-import {clipboard, shell} from "electron/common";
+import {clipboard} from "electron/common";
 import {
   BrowserWindow,
   type IpcMainEvent,
@@ -157,8 +157,6 @@ function createMainWindow(): BrowserWindow {
   }
 
   await app.whenReady();
-  // Register custom protocol handler
-  app.setAsDefaultProtocolClient("zulip");
 
   if (process.env.GDK_BACKEND !== GDK_BACKEND) {
     console.warn(
@@ -176,19 +174,13 @@ function createMainWindow(): BrowserWindow {
 
   remoteMain.initialize();
 
-  app.on("second-instance", (event, commandLine) => {
+  app.on("second-instance", () => {
     if (mainWindow) {
       if (mainWindow.isMinimized()) {
         mainWindow.restore();
       }
 
       mainWindow.show();
-    }
-    // Handle deep link when opened from protocol
-
-    const url = commandLine.find((argument) => argument.startsWith("zulip://"));
-    if (url) {
-      mainWindow.webContents.send("open-url", url);
     }
   });
 
@@ -203,24 +195,6 @@ function createMainWindow(): BrowserWindow {
   // This event is only available on macOS. Triggers when you click on the dock icon.
   app.on("activate", () => {
     mainWindow.show();
-  });
-  // Handle deep linking when app is already running (macOS)
-  app.on("open-url", async (event, url) => {
-    event.preventDefault();
-
-    const response = await dialog.showMessageBox({
-      type: "question",
-      buttons: ["Open in App", "Open in Browser"],
-      defaultId: 0,
-      message: `Do you want to open this link in the desktop app or browser?`,
-      detail: url,
-    });
-
-    if (response.response === 0) {
-      mainWindow.webContents.send("open-url", url);
-    } else {
-      await shell.openExternal(url); // Open in browser
-    }
   });
 
   app.on("web-contents-created", (_event, contents: WebContents) => {
