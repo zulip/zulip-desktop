646425afffdd8f57c1b93e40af2c0dbf94941cab
Jonas_sorgenfrei@yahoo.de <Jonas_sorgenfrei@yahoo.de>
2025-08-14 01:19:41 -0700

    link_utils: exposing whitelisted protocols to user settings
    Fixes #1284.
    Adding config option to set protocols in the config that are
    whitelisted to be opened directly.
    The behaviour is documented in docs\howto\customize-link-protocols.md.

--- app/common/config-util.ts
+++ app/common/config-util.ts
@@ -49,6 +49,28 @@ export function getConfigItem<Key extends keyof Config>(
   }
 }
 
+// The function getConfigItem stores the default in settings.json which means that
+// if the defaults change in the future, that change will not be propagated
+// to anyone having a pre-installed desktop app. We should phase out getConfigItem
+// in favour of this function and rename this to getConfigItem when it is done
+// for all existing occurences.
+export function getConfigItemWithoutSettingDefault<Key extends keyof Config>(
+  key: Key,
+  defaultValue: Config[Key],
+): z.output<ConfigSchemata[Key]> {
+  if (isConfigItemExists(key)) {
+    const typedSchemata: {
+      [Key in keyof Config]: z.ZodType<
+        z.output<ConfigSchemata[Key]>,
+        z.input<ConfigSchemata[Key]>
+      >;
+    } = configSchemata; // https://github.com/colinhacks/zod/issues/5154
+    return typedSchemata[key].parse(database.getObject<unknown>(`/${key}`));
+  }
+
+  return defaultValue;
+}
+
 // This function returns whether a key exists in the configuration file (settings.json)
 export function isConfigItemExists(key: string): boolean {
   try {
@@ -86,6 +108,7 @@ function reloadDatabase(): void {
     app.getPath("userData"),
     "/config/settings.json",
   );
+  console.log(settingsJsonPath);
   try {
     const file = fs.readFileSync(settingsJsonPath, "utf8");
     JSON.parse(file);
---
+++ docs/howto/customize-link-protocols.md
@@ -0,0 +1,35 @@
+# Customizing link protocols
+
+The Zulip app supports opening certain link protocols directly in their associated system applications. These are known as **whitelisted protocols**.
+
+## Default whitelisted protocols
+
+By default, the following protocols are whitelisted:
+
+```
+http, https, mailto, tel, sip
+```
+
+Links using these protocols are opened directly by the system.
+
+All other protocols are considered potentially unsafe and are therefore opened indirectly—through a local HTML file in your default web browser.
+
+## Extending the whitelisted protocols
+
+It is possible to customize the list of whitelisted protocols by editing the `settings.json` file located at: `userdata/Zulip/config/settings.json`
+
+To modify the list, the `whitelistedProtocols` key can be updated. For example:
+
+```json
+{
+    ...
+    "whitelistedProtocols": [
+        "http:",
+        "https:",
+        "mailto:"
+    ]
+    ...
+}
+```
+
+Note: Each protocol should include the trailing (:), e.g., "mailto:" instead of "mailto".
