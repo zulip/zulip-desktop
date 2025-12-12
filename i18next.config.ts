1c128eb1c7b32e2f56f1c09a90497a3ad98ee705
Anders Kaseorg <anders@zulip.com>
2025-12-12 13:53:55 -0800

    Upgrade dependencies, including Electron 39.2.7.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- i18next.config.ts
+++ i18next.config.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 import {defineConfig} from "i18next-cli";
 
 export default defineConfig({
@@ -6,6 +8,7 @@ export default defineConfig({
     input: ["app/**/*.ts"],
     output: "public/translations/{{language}}.json",
     functions: ["t.__", "t.__mf"],
+    defaultNS: false,
     keySeparator: false,
     nsSeparator: false,
     sort: (a, b) => (a.key < b.key ? -1 : a.key > b.key ? 1 : 0),
