f2a0c30d649b01736e71d3146b7ad5036dcee0ec
Philip Taron <philip.taron@gmail.com>
2026-05-28 08:21:26 -0700

    Upgrade prettier to 3.8.3.
    https://github.com/prettier/prettier/compare/3.7.4...3.8.3

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
           inherit (finalAttrs) pname version src;
           inherit pnpm;
           fetcherVersion = 3;
-          hash = "sha256-DkpAIesPYWCR8F/AUQliJRoPJ+X2H+NJV2/uiLc0Ljw=";
+          hash = "sha256-jPx14hcKCQlRl6elQ+bK74fpCBSZoUVw59Pjxv6MsyM=";
         };
 
         nativeBuildInputs = [
